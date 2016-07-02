2cb3f5c7e561ed57692f4634b062c0dd7897f3ab
arsenejiang <wjiang@outlook.com>
2016-07-02 10:49:36 -0700

    [58. Length of Last Word][Runtime Error]

---
+++ 58-Length-of-Last-Word/solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+        
+        int len = s.length();
+        boolean foundWord = false;
+        int lastWordLength = 0;
+        for(int i = len - 1; i >= 0; i++) {
+            if (foundWord) {
+                if (s.charAt(i) == ' ') {
+                    return lastWordLength;
+                }
+                else {
+                    lastWordLength++;
+                }
+            }
+            else {
+                if (s.charAt(i) != ' ') {
+                    foundWord = true;
+                    lastWordLength++;
+                }
+            }
+        }
+        
+        return lastWordLength;
+    }
+}
