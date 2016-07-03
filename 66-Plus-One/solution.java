5bf595c303da153be1ce455e44733bb26daba319
arsenejiang <wjiang@outlook.com>
2016-07-03 01:00:33 -0700

    [66. Plus One][Accepted]

--- 66-Plus-One/solution.java
+++ 66-Plus-One/solution.java
@@ -1,28 +1,18 @@
 public class Solution {
     public int[] plusOne(int[] digits) {
         int size = digits.length;
-        int index = 0;
-        while(index < size) {
-            if (digits[index] != 9) {
-                break;
-            }
-            index++;
-        }
-        
-        if (index == size) {
-            int[] result = new int[size+1];
-            result[0] = 1;
-            return result;
-        }
-        
-        int[] result = new int[size];
-        int carry = 1;
         for(int i = size - 1; i >= 0; i--) {
-            int sum = digits[i] + carry;
-            carry = sum / 10;
-            result[i] = sum % 10;
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            else {
+                digits[i] = 0;
+            }
         }
         
-        return result;
+        int[] res = new int[size + 1];
+        res[0] = 1;
+        return res;
     }
 }
