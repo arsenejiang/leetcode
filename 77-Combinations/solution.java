93410e31c68b20d6f123360429cef916d512a214
arsenejiang <wjiang@outlook.com>
2016-07-04 22:22:03 -0700

    [77. Combinations][Accepted]

--- 77-Combinations/solution.java
+++ 77-Combinations/solution.java
@@ -1,27 +1,13 @@
 public class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        /*
-        if (n < k || k == 0) {
-            return new ArrayList<List<Integer>>();
-        }
-        
-        if (k == n) {
+        if (k == n || k == 0) {
             List<Integer> list = new ArrayList();
-            for(int i = 1; i <= n; i++) {
+            for(int i = 1; i <= k; i++) {
                 list.add(i);
             }
             List<List<Integer>> res = new ArrayList();
             res.add(list);
             return res;
-        }
-        */
-        
-        if (k == n || k == 0) {
-            List<Integer> row = new LinkedList<>();
-            for (int i = 1; i <= k; ++i) {
-                row.add(i);
-            }
-            return new LinkedList<>(Arrays.asList(row));
         }
         
         List<List<Integer>> result = combine(n - 1, k - 1);
