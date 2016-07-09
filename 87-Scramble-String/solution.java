d29ede37b429cce46be62273ad46be3a919867ca
arsenejiang <wjiang@outlook.com>
2016-07-09 00:30:45 -0700

    [87. Scramble String][Time Limit Exceeded]

--- 87-Scramble-String/solution.java
+++ 87-Scramble-String/solution.java
@@ -10,6 +10,15 @@ public class Solution {
             return true;
         }
         
+        if (len1 == 2) {
+            if (s1.charAt(0) == s2.charAt(1) && s1.charAt(1) == s2.charAt(0)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        
         for(int i = 1; i < len1; i++) {
             if ((isScramble(s1.substring(0, i), s2.substring(0, i)) && isScramble(s1.substring(i, len1), s2.substring(i))) ||
             (isScramble(s1.substring(0, i), s2.substring(len1-i, len1)) && isScramble(s1.substring(i, len1), s2.substring(0, len1-i))))
