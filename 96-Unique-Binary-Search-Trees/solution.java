8732c1acf3f2bf84c00ef50d64f12354957ce835
arsenejiang <wjiang@outlook.com>
2016-07-10 16:40:39 -0700

    [96. Unique Binary Search Trees][Compile Error]

--- 96-Unique-Binary-Search-Trees/solution.java
+++ 96-Unique-Binary-Search-Trees/solution.java
@@ -1,32 +1,15 @@
 public class Solution {
     public int numTrees(int n) {
-        if (n <= 0) {
-            return 0;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++) {
+            for(int j = 0; j <= i - 1; j++) {
+                dp[i] += dp[j]*d[i - j - 1];
+            }
         }
         
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        map.put(1, 1);
-        return helper(1, n, map);
-    }
-    
-    private int helper(int left, int right, HashMap<Integer, Integer> map) {
-        if (left >= right) {
-            return 1;
-        }
-        
-        if (map.containsKey(right - left + 1)) {
-            return map.get(right - left + 1);
-        }
-        
-        int result = 0;
-        for(int i = left; i <= right; i++) {
-            int numLeftTrees = helper(left, i - 1, map);
-            int numRightTrees = helper(i + 1, right, map);
-            result += numLeftTrees * numRightTrees;
-        }
-        
-        map.put(right - left + 1, result);
-        
-        return result;
+        return dp[n];
     }
 }
