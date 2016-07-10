b72eeefbb0843c461a27b827f55b4dd1c62d6276
arsenejiang <wjiang@outlook.com>
2016-07-09 23:03:23 -0700

    [91. Decode Ways][Compile Error]

---
+++ 91-Decode-Ways/solution.java
@@ -0,0 +1,38 @@
+public class Solution {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0 || s[0] == '0') {
+            return 0;
+        }
+        
+        int len = s.length();
+        int[] dp = new int[len + 1];
+        dp[0] = 1;
+        if (isValid(s.substring(0, 1)) {
+            dp[1] = 1;
+        }
+        else {
+            dp[1] = 0;
+        }
+        
+        for(int i = 2; i <= n; i++) {
+            if(isValid(s.substring(i-1, i))) {
+                dp[i] += dp[i-1];
+            }
+            
+            if(isValid(s.substring(i-2, i))) {
+                dp[i] += dp[i-2];
+            }
+        }
+        
+        return dp[n];
+    }
+    
+    private boolean isValid(String s) {
+        if (s[0] == '0') {
+            return false;
+        }
+        
+        int num = Integer.parseInt(s);
+        return num >= 1 && num <= 26;
+    }
+}
