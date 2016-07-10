df86da4b171ff2e0a97232d3ebab4fd08f159556
arsenejiang <wjiang@outlook.com>
2016-07-10 16:40:49 -0700

    [96. Unique Binary Search Trees][Accepted]

--- 96-Unique-Binary-Search-Trees/solution.java
+++ 96-Unique-Binary-Search-Trees/solution.java
@@ -6,7 +6,7 @@ public class Solution {
         
         for(int i = 2; i <= n; i++) {
             for(int j = 0; j <= i - 1; j++) {
-                dp[i] += dp[j]*d[i - j - 1];
+                dp[i] += dp[j]*dp[i - j - 1];
             }
         }
         
