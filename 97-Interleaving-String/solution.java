8cfb88680d36b2f9e9df9239eaad8f34fdabb3cb
arsenejiang <wjiang@outlook.com>
2016-07-10 17:20:39 -0700

    [97. Interleaving String][Accepted]

--- 97-Interleaving-String/solution.java
+++ 97-Interleaving-String/solution.java
@@ -1,40 +1,38 @@
 public class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        return helper(s1, 0, s2, 0, s3, 0);
-    }
-    
-    private boolean helper(String s1, int i1, String s2, int i2, String s3, int i3) {
         int l1 = s1.length();
         int l2 = s2.length();
         int l3 = s3.length();
-        if (l1 - i1 + l2 - i2 != l3 - i3) {
+        if (l1 + l2 != l3) {
             return false;
         }
         
-        if (l1 == i1) {
-            return s2.substring(i2).equals(s3.substring(i3));
-        }
-        else if (l2 == i2) {
-            return s1.substring(i1).equals(s3.substring(i3));
-        }
-        
-        if(s1.charAt(i1) != s3.charAt(i3) && s2.charAt(i2) != s3.charAt(i3)) {
-            return false;
+        boolean[][] dp = new boolean[l1 + 1][l2 + 1];
+        dp[0][0] = true;
+        for(int i = 1; i <= l1; i++) {
+            if (dp[i-1][0] == true) {
+                dp[i][0] = (s1.charAt(i-1) == s3.charAt(i-1));
+            }
         }
         
-        boolean r1 = false, r2 = false;
-        if (s1.charAt(i1) == s3.charAt(i3)) {
-            r1 = helper(s1, i1 + 1, s2, i2, s3, i3 + 1);
+        for(int j = 1; j <= l2; j++) {
+            if (dp[0][j - 1] == true) {
+                dp[0][j] = (s2.charAt(j-1) == s3.charAt(j-1));
+            }
         }
         
-        if (r1 == true) {
-            return true;
+        for(int i = 1; i <= l1; i++) {
+            for(int j = 1; j <= l2; j++) {
+                if (dp[i-1][j] == true) {
+                    dp[i][j] = (s1.charAt(i-1) == s3.charAt(i + j - 1));
+                }
+                
+                if (dp[i][j] != true && dp[i][j-1] == true) {
+                    dp[i][j] = (s2.charAt(j-1) == s3.charAt(i + j - 1));
+                }
+            }
         }
         
-        if (s2.charAt(i2) == s3.charAt(i3)) {
-            r2 = helper(s1, i1, s2, i2 + 1, s3, i3 + 1);
-        }
-        
-        return r2;
+        return dp[l1][l2];
     }
 }
