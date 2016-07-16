9c054b51b6401bc149d76a1f39594936d4195c3d
arsenejiang <wjiang@outlook.com>
2016-07-15 22:58:12 -0700

    [120. Triangle][Compile Error]

--- 120-Triangle/solution.java
+++ 120-Triangle/solution.java
@@ -9,19 +9,11 @@ public class Solution {
             return 0;
         }
         
-        int[] sum = new int[row];
+        int[] sum = new int[row]{Integer.MAX_VALUE};
         int result = Integer.MAX_VALUE;
         for(int i = 0; i < row; i++) {
             for(int j = i; j >= 0; j--) {
-                if (j == 0) {
-                    sum[j] = sum[j] + triangle.get(i).get(j);
-                }
-                else if (j == i) {
-                    sum[j] = sum[j - 1] + triangle.get(i).get(j);
-                }
-                else {
-                    sum[j] = Math.min(sum[j-1], sum[j]) + triangle.get(i).get(j);
-                }
+                sum[j] = Math.min(j == 0 ? 0 : sum[j-1], sum[j]) + triangle.get(i).get(j);
                 
                 if (i == row - 1) 
                 {
