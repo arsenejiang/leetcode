37728322f70ca9fa845de90870cb9c1c8bba316d
arsenejiang <wjiang@outlook.com>
2016-07-17 23:45:54 -0700

    [130. Surrounded Regions][Accepted]

--- 130-Surrounded-Regions/solution.java
+++ 130-Surrounded-Regions/solution.java
@@ -6,53 +6,51 @@ public class Solution {
         
         int m = board.length;
         int n = board[0].length;
-        Queue<Node> q = new LinkedList();
+        for(int i = 0; i < m; i++) {
+            check(board, i, 0, m, n);
+            check(board, i, n - 1, m, n);
+        }
+        
+        for(int j = 0; j < n; j++) {
+            check(board, 0, j, m, n);
+            check(board, m - 1, j, m, n);
+        }
+        
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if (board[i][j] == 'O') {
-                    if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
-                        q.offer(new Node(i, j));
-                    }
-                    else {
-                        board[i][j] = '#';
-                    }
-                }
-            }
-        }
-        
-        while(!q.isEmpty()) {
-            Node node = q.poll();
-            int x = node.x, y = node.y;
-            for(int i = Math.max(x - 1, 0); i <= Math.min(x + 1, m - 1); i++) {
-                if (board[i][y] == '#') {
-                    board[i][y] = 'O';
-                    q.offer(new Node(i, y));
-                }
-            }
-            
-            for(int j = Math.max(y - 1, 0); j <= Math.min(y + 1, n - 1); j++) {
-                if (board[x][j] == '#') {
-                    board[x][j] = 'O';
-                    q.offer(new Node(x, j));
-                }
-            }
-        }
-        
-        for(int i = 1; i < m-1; i++) {
-            for(int j = 1; j < n-1; j++) {
-                if (board[i][j] == '#') {
                     board[i][j] = 'X';
+                }
+            }
+        }
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if (board[i][j] == 'B') {
+                    board[i][j] = 'O';
                 }
             }
         }
     }
     
-    class Node {
-        int x;
-        int y;
-        Node(int x, int y) {
-            this.x = x;
-            this.y = y;
+    private void check(char[][] board, int i, int j, int m, int n) {
+        if(board[i][j] == 'O') {
+            board[i][j] = 'B';
+            if (i > 1) {
+                check(board, i - 1, j, m, n);
+            }
+            
+            if (i < m - 2) {
+                check(board, i + 1, j, m, n);
+            }
+            
+            if (j > 1) {
+                check(board, i, j - 1, m, n);
+            }
+            
+            if (j < n - 2) {
+                check(board, i, j + 1, m, n);
+            }
         }
     }
 }
