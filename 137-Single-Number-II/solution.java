f6fffa02139fcfac407a5eb77416b0ef64049c5d
arsenejiang <wjiang@outlook.com>
2016-07-20 01:09:35 -0700

    [137. Single Number II][Accepted]

---
+++ 137-Single-Number-II/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int i = 0; i < 32; i++) {
+            int sum = 0;
+            for(int num : nums) {
+                sum += (num >> i) & 1;
+            }
+            
+            result += (sum % 3) << i;
+        }
+        
+        return result;
+    }
+}
