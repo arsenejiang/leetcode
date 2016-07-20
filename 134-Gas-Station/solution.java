88cd9cff9cda726df7e299116b3c4553735cd509
arsenejiang <wjiang@outlook.com>
2016-07-20 00:07:22 -0700

    [134. Gas Station][Compile Error]

--- 134-Gas-Station/solution.java
+++ 134-Gas-Station/solution.java
@@ -3,23 +3,21 @@ public class Solution {
         if (gas == null || cost == null || gas.length == 0 || cost.length == 0) {
             return - 1;
         } 
+        
         int len = gas.length;
         for(int i = 0; i < len; i++) {
             int curGas = 0;
-            int j = i;
-            do {
-                curGas = gas[j] - cost[j];
-                j = (j + 1) % len;
-            }while(curGas >= 0);
-            
-            if (j == i && curGas >= 0) {
-                return i;
+            int j = 0;
+            for(int j = 0; j < len; j++) {
+                int index = (i + j) % len;
+                curGas += gas[index] - cost[index];
+                if (curGas < 0) {
+                    i = index;
+                    break;
+                }
             }
             
-            if (i < j) {
-                i = j;
-                i--;
-            } 
+            return i;
         }
         
         return -1;
