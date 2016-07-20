44a0292aeb583aa110b213ddf413eda21af95aad
arsenejiang <wjiang@outlook.com>
2016-07-19 23:29:20 -0700

    [134. Gas Station][Time Limit Exceeded]

---
+++ 134-Gas-Station/solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        if (gas == null || cost == null || gas.length == 0 || cost.length == 0) {
+            return - 1;
+        } 
+        
+        int len = gas.length;
+        for(int i = 0; i < len; i++) {
+            int curGas = gas[i] - cost[i];
+            int j = (i + 1) % len;
+            while(curGas >= 0) {
+                curGas += gas[j] - cost[j];
+                j = (j + 1) % len;
+                
+                if (j == i) {
+                    return i;
+                }
+             }
+             
+             i = j - 1;
+        }
+        
+        return -1;
+    }
+}
