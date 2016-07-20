9181095771d9fdb7504a7c5f382de7c968ebc41e
arsenejiang <wjiang@outlook.com>
2016-07-20 00:39:21 -0700

    [135. Candy][Accepted]

---
+++ 135-Candy/solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public int candy(int[] ratings) {
+        if (ratings == null || ratings.length == 0) {
+            return 0;
+        }
+        int len = ratings.length;
+        int[] candies = new int[len];
+        Arrays.fill(candies, 1);
+        for(int i = 0; i < len; i++) {
+            if (i > 0 && ratings[i] > ratings[i-1]) {
+                candies[i] = Math.max(candies[i], candies[i-1] + 1);
+            }
+            
+            if (i < len - 1 && ratings[i] > ratings[i+1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        
+        for(int i = len - 1; i >= 0; i--) {
+            if (i > 0 && ratings[i] > ratings[i-1]) {
+                candies[i] = Math.max(candies[i], candies[i-1] + 1);
+            }
+            
+            if (i < len - 1 && ratings[i] > ratings[i+1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        
+        int sum = 0;
+        for(int i = 0; i < len; i++) {
+            sum += candies[i];
+        }
+        
+        return sum;
+    }
+}
