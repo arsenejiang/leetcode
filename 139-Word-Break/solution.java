39b24662c519f4cca8c4e6b797b084db2057950f
arsenejiang <wjiang@outlook.com>
2016-07-21 23:50:16 -0700

    [139. Word Break][Accepted]

---
+++ 139-Word-Break/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean wordBreak(String s, Set<String> wordDict) {
+        int len = s.length();
+        boolean[] flag = new boolean[len + 1];
+        flag[0] = true;
+        for(int i = 1; i <= len; i++) {
+            for(int j = 0; j < i; j++) {
+                if (flag[j] && wordDict.contains(s.substring(j, i))) {
+                    flag[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return flag[len];
+    }
+}
