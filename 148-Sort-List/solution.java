84180b87657d91caf410a22805d5417609afb383
arsenejiang <wjiang@outlook.com>
2016-07-23 11:39:34 -0700

    [148. Sort List][Compile Error]

--- 148-Sort-List/solution.java
+++ 148-Sort-List/solution.java
@@ -21,17 +21,24 @@ public class Solution {
     private ListNode merge(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
         ListNode p1 = l1, p2 = l2, p = dummy;
-        while(p1 != null || p2 != null) {
-            if (p1 != null && (p2 == null || p1.val <= p2.val)) {
+        while(p1 != null && p2 != null) {
+            if (p1.val <= p2.val)) {
                 p.next = p1;
                 p1 = p1.next;
-                p = p.next;
             }
-            else if (p2 != null && (p1 == null || p2.val <= p1.val)) {
+            else {
                 p.next = p2;
                 p2 = p2.next;
-                p = p.next;
             }
+            
+            p = p.next;
+        }
+        
+        if (p1 == null) {
+            p.next = p2;
+        }
+        else {
+            p.next = p1;
         }
         
         return dummy.next;
