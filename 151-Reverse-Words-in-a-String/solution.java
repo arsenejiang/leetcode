4fd553e8e53fadbedd3ffd3932ad3241218ec5a3
arsenejiang <wjiang@outlook.com>
2016-07-23 23:26:41 -0700

    [151. Reverse Words in a String][Runtime Error]

---
+++ 151-Reverse-Words-in-a-String/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public String reverseWords(String s) {
+        int len = s.length();
+        StringBuilder sb = new StringBuilder();
+        int endIndex = -1;
+        for(int i = len - 1; i >= 0; i++) {
+            if (s.charAt(i) == ' ') {
+                if(endIndex != -1) {
+                    sb.append(s.substring(i + 1, endIndex + 1));
+                    endIndex = -1;
+                }
+            }
+            else {
+                if (endIndex == -1) {
+                    endIndex = i;
+                }
+            }
+        }
+        
+        if (endIndex != -1) {
+            sb.append(s.substring(0, endIndex + 1));
+        }
+        
+        return sb.toString();
+    }
+}
