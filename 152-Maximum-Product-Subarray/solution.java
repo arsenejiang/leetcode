cf4804b3a018370508db06c97c91e118b8c1aa84
arsenejiang <wjiang@outlook.com>
2016-07-23 23:54:25 -0700

    [152. Maximum Product Subarray][Runtime Error]

---
+++ 152-Maximum-Product-Subarray/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int len = nums.length;
+        long[] dp = new long[len + 1];
+        dp[1] = nums[0];
+        
+        for(int i = 1; i < len; i++) {
+            long localMax = nums[i];
+            long product = nums[i];
+            for(int j = i - 1; j >= 0; j++) {
+                product = product * nums[j];
+                localMax = Math.max(localMax, product);
+            }
+            
+            dp[i] = Math.max(dp[i-1], localMax);
+        }
+        
+        return (int)dp[len];
+    }
+}
