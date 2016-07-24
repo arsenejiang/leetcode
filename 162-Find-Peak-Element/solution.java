c08756a6adb771ef3d819d7ab15d88892f760f79
arsenejiang <wjiang@outlook.com>
2016-07-24 13:06:24 -0700

    [162. Find Peak Element][Accepted]

--- 162-Find-Peak-Element/solution.java
+++ 162-Find-Peak-Element/solution.java
@@ -1,11 +1,18 @@
 public class Solution {
     public int findPeakElement(int[] nums) {
-        for(int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
-                return i;
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right) {
+            int mid1 = left + (right - left) / 2;
+            int mid2 = mid1 + 1;
+            if (nums[mid1] < nums[mid2]) {
+                left = mid2;
+            }
+            else {
+                right = mid1;
             }
         }
         
-        return nums.length - 1;
+        return left;
     }
 }
