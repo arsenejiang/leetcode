c6daa4ba7acfec5c9f68a89dbbadac3164733ccb
arsenejiang <wjiang@outlook.com>
2016-07-24 15:36:42 -0700

    [168. Excel Sheet Column Title][Accepted]

---
+++ 168-Excel-Sheet-Column-Title/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public String convertToTitle(int n) {
+        if (n <= 0) {
+            return "";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while(n > 0) {
+            n--;
+            sb.insert(0, (char)((n % 26) + 'A'));
+            n = n / 26;
+        }
+        
+        return sb.toString();
+    }
+}
