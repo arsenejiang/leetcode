6ce0099410abeca997e3a59bb1952e3a26d75307
arsenejiang <wjiang@outlook.com>
2016-07-24 13:39:11 -0700

    [164. Maximum Gap][Runtime Error]

---
+++ 164-Maximum-Gap/solution.java
@@ -0,0 +1,44 @@
+public class Solution {
+    public int maximumGap(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return 0;
+        }
+        
+        int len = nums.length;
+        int min = nums[0], max = nums[1];
+        for(int i = 1; i < len; i++) {
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
+        }
+        
+        int bucketLen = (int)Math.ceil((double)(max - min) / (len - 1));
+        int[] bucketMin = new int[len - 1];
+        int[] bucketMax = new int[len - 1];
+        Arrays.fill(bucketMin, Integer.MIN_VALUE);
+        Arrays.fill(bucketMax, Integer.MAX_VALUE);
+        for(int i = 0; i < len; i++) {
+            if (nums[i] == min || nums[i] == max) {
+                continue;
+            }
+            
+            int index = (nums[i] - min) / bucketLen;
+            bucketMin[index] = Math.min(bucketMin[index], nums[i]);
+            bucketMax[index] = Math.max(bucketMax[index], nums[i]);
+        }
+        
+        int maxGap = 0;
+        int previous = min;
+        for(int i = 0; i < len - 1; i++) {
+            if (bucketMin[i] == Integer.MIN_VALUE && bucketMax[i] == Integer.MAX_VALUE) {
+                continue;
+            }
+            
+            int gap = bucketMin[i] - previous;
+            maxGap = Math.max(maxGap, gap);
+            previous = bucketMax[i];
+        }
+        
+        maxGap = Math.max(maxGap, max - previous);
+        return maxGap;
+    }
+}
