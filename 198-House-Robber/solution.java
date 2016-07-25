0a78abf7751a6f340beb542ae693d0141c3d2058
arsenejiang <wjiang@outlook.com>
2016-07-25 00:32:22 -0700

    [198. House Robber][Runtime Error]

--- 198-House-Robber/solution.java
+++ 198-House-Robber/solution.java
@@ -5,10 +5,10 @@ public class Solution {
         }
         
         int len = nums.length;
-        int[] dp = new dp[len + 1];
+        int[] dp = new int[len + 1];
         dp[0] = 0;
         dp[1] = nums[0];
-        for(int i = 2; i < len; i++) {
+        for(int i = 2; i <= len; i++) {
             dp[i] = Math.max(dp[i - 2] + nums[i], dp[i-1]);
         }
         
