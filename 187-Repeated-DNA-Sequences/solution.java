764b969d477fb192c92b3bbb860458cc8b55f1e5
arsenejiang <wjiang@outlook.com>
2016-07-24 17:59:33 -0700

    [187. Repeated DNA Sequences][Compile Error]

---
+++ 187-Repeated-DNA-Sequences/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> res = new ArrayList();
+        if (s == null || s.length() < 10) {
+            return res;
+        }
+        
+        int len = s.length();
+        Map<String, Integer> map = new HashMap();
+        for(int i = 0; i <= len - 10; i++) {
+            String sub = s.substring(i, i + 10);
+            if (map.containsKey(sub) {
+                int count = map.get(sub) + 1;
+                map.put(sub, count);
+                if (count == 2) {
+                    res.add(sub);
+                }
+            }
+            else {
+                map.put(sub, 1);
+            }
+        }
+        
+        return res;
+    }
+}
