6543920270526cfc7c06546ed05293c778c634d1
arsenejiang <wjiang@outlook.com>
2016-07-24 17:39:01 -0700

    [179. Largest Number][Compile Error]

--- 179-Largest-Number/solution.java
+++ 179-Largest-Number/solution.java
@@ -4,19 +4,24 @@ public class Solution {
             return "";
         }
         
-        Comparator<int> comp = new Comparator<int>() {
+        String[] s = new String[nums.length];
+        for(int i = 0; i < nums.length; i++) {
+            s[i] = nums[i];
+        }
+        
+        Comparator<String> comp = new Comparator<String>() {
             @override
-            public int compare(int a, int b) {
+            public int compare(String a, String b) {
                 String s1 = a + b;
                 String s2 = b + a;
                 return s1.compareTo(s2);
             }
         };
         
-        Arrays.sort(nums, comp);
+        Arrays.sort(s, comp);
         StringBuilder sb = new StringBuilder();
-        for(int i = nums.length - 1; i >= 0; i++) {
-            sb.append(nums[i]);
+        for(int i = s.length - 1; i >= 0; i++) {
+            sb.append(s[i]);
         }
         
         return sb.toString();
