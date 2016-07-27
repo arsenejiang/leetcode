d7774d3116dc5e6c8ff06f7ef33ffc42d9066c88
arsenejiang <wjiang@outlook.com>
2016-07-27 00:10:26 -0700

    [202. Happy Number][Time Limit Exceeded]

--- 202-Happy-Number/solution.java
+++ 202-Happy-Number/solution.java
@@ -1,18 +1,24 @@
 public class Solution {
     public boolean isHappy(int n) {
         Set<Integer> visited = new HashSet();
-        visited.add(n);
         while(n != 1) {
-            int value = 0;
-            while (n > 0) {
-                value += (n % 10) * (n % 10);
-                n = n / 10;
-            }
-            if (visited.contains(value)) {
+            if (visited.contains(n)) {
                 return false;
             }
+            
+            n = getNext(n);
         }
         
         return true;
+    }
+    
+    private int getNext(int n) {
+        int sum = 0;
+        while(n > 0) {
+            sum += (n % 10) * (n % 10);
+            n = n / 10;
+        }
+        
+        return sum;
     }
 }
