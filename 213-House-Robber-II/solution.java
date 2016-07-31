306e526a14fc74255d97bd88a0a2ca865f4658d0
arsenejiang <wjiang@outlook.com>
2016-07-31 12:15:41 -0700

    [213. House Robber II][Accepted]

--- 213-House-Robber-II/solution.java
+++ 213-House-Robber-II/solution.java
@@ -10,21 +10,20 @@ public class Solution {
         
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
-        dp[1] = nums[1];
-        int result = Math.max(dp[0], dp[1]);
+        dp[1] = Math.max(dp[0], nums[1]);
         for(int i = 2; i < nums.length - 1; i++) {
-            dp[i] = Math.max(dp[i-2] + nums[i], nums[i-1]);
-            result = Math.max(result, dp[i]);
+            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
         }
         
-        int result = dp[nums.length - 1];
+        int result = dp[nums.length - 2];
+        
         dp[0] = 0;
         dp[1] = nums[1];
         for(int i = 2; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i-2] + nums[i], nums[i-1]);
-            result = Math.max(result, dp[i]);
+            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
         }
         
+        result = Math.max(result, dp[nums.length - 1]);
         return result;
     }
 }
