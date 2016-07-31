47375f49c880a4fecf12cc7fe4b0b35da9046882
arsenejiang <wjiang@outlook.com>
2016-07-31 16:44:44 -0700

    [215. Kth Largest Element in an Array][Accepted]

--- 215-Kth-Largest-Element-in-an-Array/solution.java
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -1,51 +1,6 @@
-import java.util.Random;
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
-        shuffle(nums);
-        k = nums.length - k;
-        int lo = 0;
-        int hi = nums.length - 1;
-        while (lo < hi) {
-            final int j = partition(nums, lo, hi);
-            if(j < k) {
-                lo = j + 1;
-            } else if (j > k) {
-                hi = j - 1;
-            } else {
-                break;
-            }
-        }
-        return nums[k];
-    }
-    
-    private int partition(int[] a, int lo, int hi) {
-
-        int i = lo;
-        int j = hi + 1;
-        while(true) {
-            while(i < hi && a[++i] < a[lo]);
-            while(j > lo && a[lo] < a[--j]);
-            if(i >= j) {
-                break;
-            }
-            swap(a, i, j);
-        }
-        swap(a, lo, j);
-        return j;
-    }
-
-    private void shuffle(int a[]) {
-
-        final Random random = new Random();
-        for(int ind = 1; ind < a.length; ind++) {
-            final int r = random.nextInt(ind + 1);
-            swap(a, ind, r);
-        }
-    }
-    
-    private void swap(int[] a, int i, int j) {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
+        Arrays.sort(nums);
+        return nums[nums.length - k];
     }
 }
