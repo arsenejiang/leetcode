530203afa29ffa6823e24974db1a23afd5ed71b9
arsenejiang <wjiang@outlook.com>
2016-07-31 16:39:45 -0700

    [215. Kth Largest Element in an Array][Compile Error]

--- 215-Kth-Largest-Element-in-an-Array/solution.java
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -1,46 +1,44 @@
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int left = 0;
-        int right = nums.length - 1;
-        while(true) {
-            int pos = partition(nums, left, right);
-            if (pos == k - 1) {
-                return nums[pos];
-            }
-            
-            if (pos > k - 1) {
-                right = pos - 1;
-            }
-            else {
-                left = pos + 1;
+        shuffle(nums);
+        k = nums.length - k;
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo < hi) {
+            final int j = partition(nums, lo, hi);
+            if(j < k) {
+                lo = j + 1;
+            } else if (j > k) {
+                hi = j - 1;
+            } else {
+                break;
             }
         }
+        return nums[k];
     }
     
-    private int partition(int[] nums, int low, int high) {
-        int pivot = nums[low], i = low + 1, j = high;
-        while(i <= j) {
-            if (nums[i] < pivot && nums[j] > pivot) {
-                swap(nums, i, j);
-                i++;
-                j--;
+    private int partition(int[] a, int lo, int hi) {
+
+        int i = lo;
+        int j = hi + 1;
+        while(true) {
+            while(i < hi && less(a[++i], a[lo]));
+            while(j > lo && less(a[lo], a[--j]));
+            if(i >= j) {
+                break;
             }
-            
-            if (nums[i] >= pivot) {
-                i++;
-            }
-            if (nums[j] <= pivot) {
-                j--;
-            }
+            exch(a, i, j);
         }
-        
-        swap(nums, low, j);
+        exch(a, lo, j);
         return j;
     }
-    
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+
+    private void shuffle(int a[]) {
+
+        final Random random = new Random();
+        for(int ind = 1; ind < a.length; ind++) {
+            final int r = random.nextInt(ind + 1);
+            exch(a, ind, r);
+        }
     }
 }
