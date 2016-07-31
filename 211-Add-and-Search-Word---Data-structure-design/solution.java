815b2f93a4ddd1b5944c6cbed4f45ef2140fab50
arsenejiang <wjiang@outlook.com>
2016-07-30 23:12:42 -0700

    [211. Add and Search Word - Data structure design][Compile Error]

---
+++ 211-Add-and-Search-Word---Data-structure-design/solution.java
@@ -0,0 +1,55 @@
+public class WordDictionary {
+    private TrieNode root = new TrieNode();
+    // Adds a word into the data structure.
+    public void addWord(String word) {
+        TrieNode r = root;
+        for(int i = 0; i < word.length; i++) {
+            char c = word.charAt(i);
+            if (r.children[c - 'a'] == null) {
+                r.children[c - 'a'] = new TrieNode();
+            }
+            r = r.children[c - 'a'];
+        }
+    }
+
+    // Returns if the word is in the data structure. A word could
+    // contain the dot character '.' to represent any one letter.
+    public boolean search(String word) {
+        return search(word.toCharArray(), 0, root);
+    }
+    
+    private boolean search(char[] str, int index, TrieNode parent) {
+        if (index == str.length) {
+            return parent.isWord;
+        }
+        
+        if (str[index] == '.') {
+            for(int i = 0; i < 26; i++) {
+                if (parent.children[i] != null) {
+                    if (search(str, index + 1, parent.children[i])) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        else {
+            if (parent.children[c - str[index]] != null) {
+                return search(str, index + 1, parent.child[c - str[index]]);
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
+
+public class TrieNode {
+    public boolean isWord;
+    public TrieNode[] children = new TrieNode[26];
+}
+
+// Your WordDictionary object will be instantiated and called as such:
+// WordDictionary wordDictionary = new WordDictionary();
+// wordDictionary.addWord("word");
+// wordDictionary.search("pattern");
