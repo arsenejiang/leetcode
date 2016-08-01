4fd2135f1e2eabae684a1a3136f179331d535fdb
arsenejiang <wjiang@outlook.com>
2016-07-31 17:15:42 -0700

    [216. Combination Sum III][Accepted]

--- 216-Combination-Sum-III/solution.java
+++ 216-Combination-Sum-III/solution.java
@@ -7,16 +7,14 @@ public class Solution {
     }
     
     private void helper(List<List<Integer>> res, List<Integer> cur, int start, int k, int sum) {
-        if (sum < start || cur.size() == k) {
-            if (sum == 0 && cur.size() == k) {
-                res.add(new ArrayList(cur));
-            }
+        if (sum == 0 && k == 0) {
+            res.add(new ArrayList(cur));
             return;
         }
         
         for(int i = start; i <= sum && i <= 9; i++) {
             cur.add(i);
-            helper(res, cur, i + 1, k, sum - i);
+            helper(res, cur, i + 1, k - 1, sum - i);
             cur.remove(cur.size() - 1);
         }
     }
