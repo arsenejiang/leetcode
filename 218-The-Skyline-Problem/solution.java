fe74150d3b539c908e6ad11b092526532a6ad68f
arsenejiang <wjiang@outlook.com>
2016-07-31 23:31:38 -0700

    [218. The Skyline Problem][Accepted]

---
+++ 218-The-Skyline-Problem/solution.java
@@ -0,0 +1,65 @@
+public class Solution {
+    public List<int[]> getSkyline(int[][] buildings) {
+        List<int[]> res = new ArrayList();
+        if (buildings == null || buildings.length == 0 || buildings[0].length == 0) {
+            return res;
+        }
+        
+        List<Edge> edges = new ArrayList();
+        for(int i = 0; i < buildings.length; i++) {
+            edges.add(new Edge(buildings[i][0], buildings[i][2], true));
+            edges.add(new Edge(buildings[i][1], buildings[i][2], false));
+        }
+        
+        Comparator<Edge> comp = new Comparator<Edge>() {
+            @Override
+            public int compare(Edge a, Edge b) {
+                if (a.x != b.x) {
+                    return a.x - b.x;
+                }
+                
+                if (a.isLeft && b.isLeft) {
+                    return b.height - a.height;
+                }
+                
+                if (!a.isLeft && !b.isLeft) {
+                    return a.height - b.height;
+                }
+                
+                return a.isLeft ? -1 : 1;
+            }
+        };
+        
+        Collections.sort(edges, comp);
+        PriorityQueue<Integer> pq = new PriorityQueue(11, Collections.reverseOrder());
+        pq.offer(0);
+        int prev = 0;
+        for(Edge e : edges) {
+            if (e.isLeft) {
+                pq.offer(e.height);
+            }
+            else {
+                pq.remove(e.height);
+            }
+            
+            int cur = pq.peek();
+            if (cur != prev) {
+                res.add(new int[]{e.x, cur});
+                prev = cur;
+            }
+        }
+        
+        return res;
+    }
+}
+
+class Edge {
+    int x;
+    int height;
+    boolean isLeft;
+    Edge(int x, int height, boolean isLeft) {
+        this.x = x;
+        this.height = height;
+        this.isLeft = isLeft;
+    }
+}
