3eefaea3b67f78723ddd19578da3ee59bd7b68f7
arsenejiang <wjiang@outlook.com>
2016-08-06 10:36:53 -0700

    [222. Count Complete Tree Nodes][Time Limit Exceeded]

---
+++ 222-Count-Complete-Tree-Nodes/solution.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
