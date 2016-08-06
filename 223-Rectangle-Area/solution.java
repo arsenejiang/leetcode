cb0783cfe0b13dd274ab2d306023ad55eb3d4abf
arsenejiang <wjiang@outlook.com>
2016-08-06 11:07:18 -0700

    [223. Rectangle Area][Accepted]

---
+++ 223-Rectangle-Area/solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        int area = (C - A) * (D - B) + (G - E) * (H - F);
+        int I = Math.max(A, E);
+        int J = Math.max(B, F);
+        int K = Math.min(C, G);
+        int L = Math.min(D, H);
+        if (K > I && L > J) {
+            area -= (K - I) * (L - J);
+        }
+        
+        return area;
+    }
+}
