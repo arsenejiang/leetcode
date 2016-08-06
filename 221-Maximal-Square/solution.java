6aaeccef47b2559cbac9d40ab65033cbe0418d1a
arsenejiang <wjiang@outlook.com>
2016-08-06 00:27:37 -0700

    [221. Maximal Square][Accepted]

--- 221-Maximal-Square/solution.java
+++ 221-Maximal-Square/solution.java
@@ -24,21 +24,28 @@ public class Solution {
     */
     
     public int maximalSquare(char[][] matrix) {
-        int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
-        int[] dp = new int[cols + 1];
-        int maxsqlen = 0, prev = 0;
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= cols; j++) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] dp = new int[n + 1];
+        int result = 0, prev = 0;
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
                 int temp = dp[j];
-                if (matrix[i - 1][j - 1] == '1') {
-                    dp[j] = Math.min(Math.min(dp[j - 1], prev), dp[j]) + 1;
-                    maxsqlen = Math.max(maxsqlen, dp[j]);
-                } else {
+                if (matrix[i-1][j-1] == '1') {
+                    dp[j] = Math.min(Math.min(dp[j], dp[j-1]), prev) + 1;
+                    result = Math.max(dp[j], result);
+                }
+                else {
                     dp[j] = 0;
                 }
                 prev = temp;
             }
         }
-        return maxsqlen * maxsqlen;
+        
+        return result * result;
     }
 }
