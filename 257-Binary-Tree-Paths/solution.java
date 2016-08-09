7547d7046a89fe81bbfe344f6932a83dca705b8c
arsenejiang <wjiang@outlook.com>
2016-08-09 00:24:15 -0700

    [257. Binary Tree Paths][Accepted]

--- 257-Binary-Tree-Paths/solution.java
+++ 257-Binary-Tree-Paths/solution.java
@@ -8,6 +8,7 @@
  * }
  */
 public class Solution {
+    /*
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> res = new ArrayList();
         if (root == null) {
@@ -34,5 +35,29 @@ public class Solution {
         }
         
         return res;
+    }
+    */
+    
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList();
+        if (root != null) {
+            helper(root, "", res);
+        }
+        
+        return res;
+    }
+    
+    private void helper(TreeNode root, String path, List<String> res) {
+        if (root.left == null && root.right == null) {
+            res.add(path + root.val);
+        }
+        
+        if (root.left != null) {
+            helper(root.left, path + root.val + "->", res);
+        }
+        
+        if (root.right != null) {
+            helper(root.right, path + root.val + "->", res);
+        }
     }
 }
