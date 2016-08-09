48a27d94c50d9b47f19e7668b5b8764d97de22c0
arsenejiang <wjiang@outlook.com>
2016-08-08 23:54:44 -0700

    [257. Binary Tree Paths][Compile Error]

---
+++ 257-Binary-Tree-Paths/solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList();
+        if (root == null) {
+            return res;
+        }
+        
+        if (root.left == null && root.right == null) {
+            res.add(root.val);
+            return;
+        }
+        
+        if (root.left != null) {
+            List<String> left = binaryTreePaths(root.left);
+            for(String l : left) {
+                res.add(root.val + "->" + l);
+            }
+        }
+        
+        if (root.right != null) {
+            List<String> right = binaryTreePaths(root.right);
+            for(String r : right) {
+                res.add(root.val + "->" + r);
+            }
+        }
+        
+        return res;
+    }
+}
