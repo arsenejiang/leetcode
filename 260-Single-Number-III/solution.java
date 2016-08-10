ad571e47160eadc6a43e1e8b3c7df94078f07f8f
arsenejiang <wjiang@outlook.com>
2016-08-09 22:50:01 -0700

    [260. Single Number III][Compile Error]

---
+++ 260-Single-Number-III/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int[] singleNumber(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return new int[0];
+        }
+        
+        int xor = 0;
+        for(int i : nums) {
+            xor = xor ^ i;
+        }
+        
+        int lastOne = xor & (-xor);
+        
+        int[] res = new int[2];
+        for(int i : nums) {
+            if (i & lastOne == 0) {
+                res[0] ^= i;
+            }
+            else {
+                res[1] ^= i;
+            }
+        }
+        
+        return res;
+    }
+}
