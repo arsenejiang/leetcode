4694ceef963b1a83b585b7e0faed71e756ec656e
arsenejiang <wjiang@outlook.com>
2016-08-12 23:29:17 -0700

    [274. H-Index][Compile Error]

---
+++ 274-H-Index/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int hIndex(int[] citations) {
+        if (citations == null || citations.length == 0) {
+            return 0;
+        }
+        
+        Arrays.sort(citations);
+        int result = 0;
+        for(int i = citations.length - 1; i >= 0; i--) {
+            int curH = Math.min(citiations[i], citations.length - 1);
+            if (curH > result) {
+                result = curH;
+            }
+        }
+        
+        return result;
+    }
+}
