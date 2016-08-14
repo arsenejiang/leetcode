dbaaf141b9bada18fd6d687bcac8a57ccd16526a
arsenejiang <wjiang@outlook.com>
2016-08-14 13:44:23 -0700

    [282. Expression Add Operators][Runtime Error]

---
+++ 282-Expression-Add-Operators/solution.java
@@ -0,0 +1,76 @@
+public class Solution {
+    private final static char[] operators = new char[] {'*', '+', '-'};
+    
+    public List<String> addOperators(String num, int target) {
+        List<String> res = new ArrayList<String>();
+        if (num == null || num.length() == 0) {
+            return res;
+        }
+        
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        Set<String> set = helper(num, 0, num.length() - 1, map);
+        
+        for(String s : set) {
+            if (map.get(s) == target) {
+                res.add(s);
+            }
+        }
+        
+        return res;
+    }
+    
+    private Set<String> helper(String num, int start, int end, Map<String, Integer> map) {
+        Set<String> res = new HashSet();
+        if (start > end) {
+            return res;
+        }
+        
+        String s = num.substring(start, end + 1);
+        if (!map.containsKey(s)) {
+            map.put(s, Integer.parseInt(s));
+        }
+        res.add(s);
+        
+        for(int i = start; i <= end - 1; i++) {
+            Set<String> left = helper(num, start, i, map);
+            Set<String> right = helper(num, i + 1, end, map);
+            for(String l : left) {
+                int lv = map.get(l);
+                for(String r : right) {
+                    int rv = map.get(r);
+                    String temp;
+                    for(char o : operators) {
+                        switch(o) {
+                            case '*' :
+                                temp = l + "*" + r;
+                                if (!res.contains(temp)) {
+                                    res.add(temp);
+                                    if (!map.containsKey(temp)) {
+                                        map.put(temp, lv*rv);
+                                    }
+                                }
+                            case '+' :
+                                temp = l + "+" + r;
+                                if (!res.contains(temp)) {
+                                    res.add(temp);
+                                    if (!map.containsKey(temp)) {
+                                        map.put(temp, lv+rv);
+                                    }
+                                }
+                            case '-' :
+                                temp = l + "-" + r;
+                                if (!res.contains(temp)) {
+                                    res.add(temp);
+                                    if (!map.containsKey(temp)) {
+                                        map.put(temp, lv-rv);
+                                    }
+                                }
+                        }
+                    }
+                }
+            }
+        }
+        
+        return res;
+    }
+}
