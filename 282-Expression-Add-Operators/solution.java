f1e13ce06ea34c7e145e89e22028b410a4b29d8f
arsenejiang <wjiang@outlook.com>
2016-08-14 16:26:08 -0700

    [282. Expression Add Operators][Accepted]

--- 282-Expression-Add-Operators/solution.java
+++ 282-Expression-Add-Operators/solution.java
@@ -1,76 +1,40 @@
 public class Solution {
-    private final static char[] operators = new char[] {'*', '+', '-'};
-    
     public List<String> addOperators(String num, int target) {
         List<String> res = new ArrayList<String>();
         if (num == null || num.length() == 0) {
             return res;
         }
         
-        Map<String, Long> map = new HashMap<String, Long>();
-        Set<String> set = helper(num, 0, num.length() - 1, map);
-        
-        for(String s : set) {
-            if (map.get(s) == target) {
-                res.add(s);
-            }
-        }
-        
+        helper(num, target, 0, 0, 0, "", res);
         return res;
     }
     
-    private Set<String> helper(String num, int start, int end, Map<String, Long> map) {
-        Set<String> res = new HashSet();
-        if (start > end) {
-            return res;
-        }
-        
-        String s = num.substring(start, end + 1);
-        if (!map.containsKey(s)) {
-            map.put(s, Long.parseLong(s));
-        }
-        res.add(s);
-        
-        for(int i = start; i <= end - 1; i++) {
-            Set<String> left = helper(num, start, i, map);
-            Set<String> right = helper(num, i + 1, end, map);
-            for(String l : left) {
-                int lv = map.get(l);
-                for(String r : right) {
-                    int rv = map.get(r);
-                    String temp;
-                    for(char o : operators) {
-                        switch(o) {
-                            case '*' :
-                                temp = l + "*" + r;
-                                if (!res.contains(temp)) {
-                                    res.add(temp);
-                                    if (!map.containsKey(temp)) {
-                                        map.put(temp, lv*rv);
-                                    }
-                                }
-                            case '+' :
-                                temp = l + "+" + r;
-                                if (!res.contains(temp)) {
-                                    res.add(temp);
-                                    if (!map.containsKey(temp)) {
-                                        map.put(temp, lv+rv);
-                                    }
-                                }
-                            case '-' :
-                                temp = l + "-" + r;
-                                if (!res.contains(temp)) {
-                                    res.add(temp);
-                                    if (!map.containsKey(temp)) {
-                                        map.put(temp, lv-rv);
-                                    }
-                                }
-                        }
-                    }
-                }
-            }
-        }
-        
-        return res;
+    private void helper(String num, int target, int startIndex, long curVal, long prevNum, String path, List<String> res) {
+          int len = num.length();
+          if (startIndex == len) {
+              if (curVal == target) {
+                  res.add(path);
+              }
+              return;
+          }
+          
+          long val = 0;
+          int pLen = path.length();
+          for(int i = startIndex; i < len; i++) {
+              if (i > startIndex && num.charAt(startIndex) == '0') {
+                  break;
+              }
+              
+              String cur = num.substring(startIndex, i + 1);
+              val = val * 10 + num.charAt(i) - '0';
+              if (pLen > 0) {
+                  helper(num, target, i + 1, curVal + val, val, path + "+" + cur, res);
+                  helper(num, target, i + 1, curVal - val, -val, path + "-" + cur, res);
+                  helper(num, target, i + 1, curVal - prevNum + prevNum * val, prevNum * val, path + "*" + cur, res);
+              }
+              else {
+                  helper(num, target, i + 1, curVal + val, val, path + cur, res);
+              }
+          }
     }
 }
