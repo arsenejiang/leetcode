3e6184364d8ca3c8b4d2ca0d49a8d864d884dfdd
arsenejiang <wjiang@outlook.com>
2016-08-15 23:36:37 -0700

    [295. Find Median from Data Stream][Compile Error]

---
+++ 295-Find-Median-from-Data-Stream/solution.java
@@ -0,0 +1,34 @@
+public class MedianFinder {
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+    private PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
+    
+    // Adds a number into the data structure.
+    public void addNum(int num) {
+        if (maxHeap.size() <= minHeap.size()) {
+            minHeap.offer(num);
+            maxHeap.poll(minHeap.poll());
+        }
+        else {
+            maxHeap.offer(num);
+            minHeap.poll(maxHeap.poll());
+        }
+    }
+
+    // Returns the median of current data stream
+    public double findMedian() {
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.peek() + minheap()) / 2.0;
+        }
+        else if (maxHeap.size() > minHeap.size()) {
+            return maxHeap.peek();
+        }
+        else {
+            return minHeap.peek();
+        }
+    }
+};
+
+// Your MedianFinder object will be instantiated and called as such:
+// MedianFinder mf = new MedianFinder();
+// mf.addNum(1);
+// mf.findMedian();
