fa7cb661564b559b2f66fff3ccdf26ef12cf5fa8
arsenejiang <wjiang@outlook.com>
2016-08-21 15:27:46 -0700

    [306. Additive Number][Accepted]

---
+++ 306-Additive-Number/solution.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public boolean isAdditiveNumber(String num) {
+        int len = num.length();
+        for(int i = 1; i <= len / 2; i++) {
+            for(int j = i + 1; j <= len / 2 + i && j < len; j++) {
+                if (isValid(num, i, j)) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    private boolean isValid(String num, int i, int j) {
+        if (num.charAt(0) == '0' && i > 1) {
+            return false;
+        }
+        
+        if (num.charAt(i) == '0' && j > i + 1) {
+            return false;
+        }
+        
+        String sum;
+        Long x1 = Long.parseLong(num.substring(0, i));
+        Long x2 = Long.parseLong(num.substring(i, j));
+        int len = num.length();
+        for(int start = j; start < len; start += sum.length()) {
+            x2 = x1 + x2;
+            x1 = x2 - x1;
+            sum = x2.toString();
+            if (!num.startsWith(sum, start)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
