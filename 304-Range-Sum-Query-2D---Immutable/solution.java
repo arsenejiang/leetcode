dee93756af6340687fb13eac417e9bec22f13ab5
arsenejiang <wjiang@outlook.com>
2016-08-21 12:52:23 -0700

    [304. Range Sum Query 2D - Immutable][Time Limit Exceeded]

--- 304-Range-Sum-Query-2D---Immutable/solution.java
+++ 304-Range-Sum-Query-2D---Immutable/solution.java
@@ -5,7 +5,7 @@ public class NumMatrix {
         int n = m > 0 ? matrix[0].length : 0;
         dp = new int[m+1][n+1];
         for(int i = 1; i < m + 1; i++) {
-            for(int j = 1; j < n + 1; i++) {
+            for(int j = 1; j < n + 1; j++) {
                 dp[i][j] = dp[i-1][j] + dp[i][j-1] + matrix[i-1][j-1] - dp[i-1][j-1];
             }
         }
