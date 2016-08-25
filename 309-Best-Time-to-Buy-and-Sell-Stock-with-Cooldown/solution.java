0f036239cfdf8bfbe8a8d363d28a0225ccdd1573
arsenejiang <wjiang@outlook.com>
2016-08-25 16:26:48 -0700

    [309. Best Time to Buy and Sell Stock with Cooldown][Runtime Error]

--- 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/solution.java
+++ 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/solution.java
@@ -21,7 +21,7 @@ public class Solution {
         buy[0] = -prices[0];
         sell[0] = 0;
         buy[1] = Math.max(buy[0], -prices[1]);
-        sell[1] = Math.max(0, buy[0] + price[1]);
+        sell[1] = Math.max(0, buy[0] + prices[1]);
         for(int i = 2; i < len; i++) {
             buy[i] = Math.max(buy[i-1], sell[i-2] - prices[i]);
             sell[i] = Math.max(sell[i-1], buy[i-1] + prices[i]);
