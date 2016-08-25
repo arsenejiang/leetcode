ba39caafc983221fa38e3fc3b3300c2c1e9e565f
arsenejiang <wjiang@outlook.com>
2016-08-25 16:42:55 -0700

    [334. Increasing Triplet Subsequence][Accepted]

---
+++ 334-Increasing-Triplet-Subsequence/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return false;
+        }
+        
+        int firstMin = Integer.MAX_VALUE, secondMin = Integer.MAX_VALUE; 
+        for(int num : nums) {
+            if (num <= firstMin) {
+                firstMin = num;
+            }
+            else if (num <= secondMin) {
+                secondMin = num;
+            }
+            else {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
