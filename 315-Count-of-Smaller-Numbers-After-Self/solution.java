370b40b71454b82f765157a82d7daf251a778d75
arsenejiang <wjiang@outlook.com>
2016-08-25 23:42:11 -0700

    [315. Count of Smaller Numbers After Self][Accepted]

---
+++ 315-Count-of-Smaller-Numbers-After-Self/solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+        List<Integer> res = new ArrayList();
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        
+        int len = nums.length;
+        List<Integer> sorted = new ArrayList();
+        sorted.add(nums[len - 1]);
+        res.add(0);
+        for(int i = len - 2; i >= 0; i--) {
+            int index = findIndex(sorted, nums[i]);
+            sorted.add(index, nums[i]);
+            res.add(0, index);
+        }
+        
+        return res;
+    }
+    
+    private int findIndex(List<Integer> sorted, int target) {
+        int left = 0;
+        int right = sorted.size() - 1;
+        while(left <= right) {
+            int mid = (right - left)/2 + left;
+            if (sorted.get(mid) >= target) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+}
