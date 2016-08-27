add35ff73c6b931652a7088792f90e52542f2763
arsenejiang <wjiang@outlook.com>
2016-08-27 00:13:39 -0700

    [322. Coin Change][Accepted]

--- 322-Coin-Change/solution.java
+++ 322-Coin-Change/solution.java
@@ -1,4 +1,6 @@
 public class Solution {
+    // backtracking TLE
+    // need to use dp
     public int coinChange(int[] coins, int amount) {
         if (coins == null || coins.length == 0 || amount < 0) {
             return -1;
@@ -8,29 +10,21 @@ public class Solution {
             return 0;
         }
         
+        int[] dp = new int[amount+1];
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++) {
+            dp[i] = -1;
+        }
+        
         Arrays.sort(coins);
-        int[] res = new int[1];
-        res[0] = Integer.MAX_VALUE;
-        helper(coins, coins.length - 1, 0, amount, res);
-        return res[0] == Integer.MAX_VALUE ? -1 : res[0];
-    }
-    
-    private void helper(int[] coins, int index, int count, int amount, int[] res) {
-        if (amount < 0) {
-            return;
-        }
-        
-        if (amount == 0) {
-            if (count < res[0]) {
-                res[0] = count;
-            }
-            return;
-        }
-        
-        for(int i = index; i >= 0; i--) {
-            if (amount >= coins[i] && count + 1 < res[0]) {
-                helper(coins, i, count + 1, amount - coins[i], res);
+        for(int i = 1; i <= amount; i++) {
+            for(int j = coins.length - 1; j >= 0; j--) {
+                if (i >= coins[j] && dp[i - coins[j]] != -1) {
+                    dp[i] = (dp[i] == - 1) ? dp[i - coins[j]] + 1 : Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
             }
         }
+        
+        return dp[amount];
     }
 }
