15f910f6df551efe5292cd55fb470bbe411fa15d
arsenejiang <wjiang@outlook.com>
2016-08-27 00:00:44 -0700

    [322. Coin Change][Time Limit Exceeded]

---
+++ 322-Coin-Change/solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if (coins == null || coins.length == 0 || amount < 0) {
+            return -1;
+        }
+        
+        if (amount == 0) {
+            return 0;
+        }
+        
+        Arrays.sort(coins);
+        int[] res = new int[1];
+        res[0] = Integer.MAX_VALUE;
+        helper(coins, coins.length - 1, 0, amount, res);
+        return res[0] == Integer.MAX_VALUE ? -1 : res[0];
+    }
+    
+    private void helper(int[] coins, int index, int count, int amount, int[] res) {
+        if (amount < 0) {
+            return;
+        }
+        
+        if (amount == 0) {
+            if (count < res[0]) {
+                res[0] = count;
+            }
+            return;
+        }
+        
+        for(int i = index; i >= 0; i--) {
+            if (amount >= coins[i]) {
+                helper(coins, i, count + 1, amount - coins[i], res);
+            }
+        }
+    }
+}
