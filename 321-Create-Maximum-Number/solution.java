3cce6668ad958d7beebd27e65b302a6daa14ab89
arsenejiang <wjiang@outlook.com>
2016-08-26 22:59:16 -0700

    [321. Create Maximum Number][Compile Error]

--- 321-Create-Maximum-Number/solution.java
+++ 321-Create-Maximum-Number/solution.java
@@ -3,7 +3,7 @@ public class Solution {
         int n1 = nums1.length;
         int n2 = nums2.length;
         int[] res = new int[k];
-        for(int i = 0; i < k; i++) {
+        for(int i = 0; i <= k; i++) {
             if (i <= n1 && k - i <= n2) {
                 int[] tmp = merge(maxArray(nums1, i), maxArray(nums2, k - i));
                 if (greater(tmp, res)) {
@@ -15,18 +15,23 @@ public class Solution {
         return res;
     }
     
-    private boolean greater(int[] nums1, int[] nums2) {
-        int i = 0, j = 0;
-        while(nums1[i] == nums2[j]) {
+    private boolean greater(int[] nums1, int i, int[] nums2, int j) {
+        while(i < nums1.length && j < nums2.length && nums1[i] == nums2[j]) {
             i++;
             j++;
         }
         
-        if (i == nums1.length) {
+        if (j == nums2.length) {
+            return true;
+        }
+        else if (i == nums2.length) {
+            return false;
+        }
+        else if (nums[i] > nums[j]) {
             return true;
         }
         else {
-            return nums1[i] > nums2[j];
+            return false;
         }
     }
     
@@ -36,25 +41,21 @@ public class Solution {
         int[] res = new int[n1+n2];
         int i = 0, j = 0, k = 0;
         while(k < n1 + n2) {
-            if (i < n1 && j < n2) {
-                res[k++] = nums1[i] >= nums2[j] ? nums1[i++] : nums2[j++];
-            }
-            else if (i == n1) {
-                res[k++] = nums2[j++];
-            }
-            else {
-                res[k++] = nums1[i++];
-            } 
+            res[k++] = greater(nums1, i, nums2, j) ? nums1[i++] : nums2[j++];
         }
         
         return res;
     }
     
     private int[] maxArray(int[] nums, int k) {
+        if (k == nums.length) {
+            return nums;
+        }
+        
         int[] res = new int[k];
         int n = nums.length;
         for(int i = 0, j = 0; i < n; i++) {
-            while(n - i + j > k && j > 0 && nums[j-1] < nums[i]) {
+            while(n - i + j > k && j > 0 && res[j-1] < nums[i]) {
                 j--;
             }
             
