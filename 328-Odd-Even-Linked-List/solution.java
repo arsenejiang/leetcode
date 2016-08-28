29c0c7a91d950966da536812179c9481d2f08d9f
arsenejiang <wjiang@outlook.com>
2016-08-27 23:37:40 -0700

    [328. Odd Even Linked List][Accepted]

--- 328-Odd-Even-Linked-List/solution.java
+++ 328-Odd-Even-Linked-List/solution.java
@@ -7,6 +7,7 @@
  * }
  */
 public class Solution {
+    /*
     public ListNode oddEvenList(ListNode head) {
         if (head == null || head.next == null || head.next.next == null) {
             return head;
@@ -28,5 +29,24 @@ public class Solution {
         
         oddCur.next = evenDummyHead.next;
         return oddDummyHead.next;
+    }
+    */
+    
+    // clean solution
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        
+        ListNode odd = head, even = head.next, evenHead = even;
+        while(even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        
+        odd.next = evenHead;
+        return head;
     }
 }
