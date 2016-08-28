c73f5843164a0f2b1cb94b0803c5bdfd3fad468a
arsenejiang <wjiang@outlook.com>
2016-08-27 23:20:06 -0700

    [327. Count of Range Sum][Accepted]

--- 327-Count-of-Range-Sum/solution.java
+++ 327-Count-of-Range-Sum/solution.java
@@ -1,27 +1,96 @@
 public class Solution {
     public int countRangeSum(int[] nums, int lower, int upper) {
-        int n = nums.length;
-        long[] sums = new long[n + 1];
-        for (int i = 0; i < n; ++i)
-            sums[i + 1] = sums[i] + nums[i];
-        return countWhileMergeSort(sums, 0, n + 1, lower, upper);
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int len = nums.length;
+        long[] sum = new long[len + 1];
+        sum[0] = 0;
+        for(int i = 0; i < len; i++) {
+            sum[i + 1] += sum[i] + nums[i];
+        }
+        
+        int result = 0;
+        TreeNode root = new TreeNode(sum[0]);
+        for(int i = 1; i <= len; i++) {
+            result += rangeSize(root, sum[i] - upper, sum[i] - lower);
+            insert(root, sum[i]);
+        }
+        
+        return result;
     }
     
-    private int countWhileMergeSort(long[] sums, int start, int end, int lower, int upper) {
-        if (end - start <= 1) return 0;
-        int mid = (start + end) / 2;
-        int count = countWhileMergeSort(sums, start, mid, lower, upper) 
-                  + countWhileMergeSort(sums, mid, end, lower, upper);
-        int j = mid, k = mid, t = mid, r = 0;
-        long[] cache = new long[end - start];
-        for (int i = start; i < mid; ++i, ++r) {
-            while (k < end && sums[k] - sums[i] < lower) k++;
-            while (j < end && sums[j] - sums[i] <= upper) j++;
-            while (t < end && sums[t] < sums[i]) cache[r++] = sums[t++]; //start merging
-            cache[r] = sums[i];
-            count += j - k;
+    private int rangeSize(TreeNode root, long lower, long upper) {
+        int total = root.leftSize + root.count + root.rightSize;
+        int smaller = countSmaller(root, lower);
+        int larger = countLarger(root, upper);
+        return total - smaller - larger;
+    }
+    
+    private int countSmaller(TreeNode root, long val) {
+        if (root == null) {
+            return 0;
         }
-        System.arraycopy(cache, 0, sums, start, r);
-        return count;
+        else if (root.val == val) {
+            return root.leftSize;
+        }
+        else if (root.val < val) {
+            return root.leftSize + root.count + countSmaller(root.right, val);
+        }
+        else {
+            return countSmaller(root.left, val);
+        }
+    }
+    
+    private int countLarger(TreeNode root, long val) {
+        if (root == null) {
+            return 0;
+        }
+        else if (root.val == val) {
+            return root.rightSize;
+        }
+        else if (root.val < val) {
+            return countLarger(root.right, val);
+        }
+        else {
+            return root.rightSize + root.count + countLarger(root.left, val);
+        }
+    }
+    
+    private TreeNode insert(TreeNode root, long val) {
+        if (root == null) {
+            root = new TreeNode(val);
+        }
+        else if (root.val == val) {
+            root.count++;
+        } 
+        else if (root.val > val) {
+            root.leftSize++;
+            root.left = insert(root.left, val);
+        }
+        else {
+            root.rightSize++;
+            root.right = insert(root.right, val);
+        }
+        
+        return root;
+    }
+    
+    private class TreeNode{
+        long val;
+        int count;
+        int leftSize;
+        int rightSize;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(long val) {
+            this.val = val;
+            this.count = 1;
+            this.leftSize = 0;
+            this.rightSize = 0;
+            this.left = null;
+            this.right = null;
+        }
     }
 }
