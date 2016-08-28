0baea1f6bfd492331f7be3a5011d38d4ace5aa36
arsenejiang <wjiang@outlook.com>
2016-08-28 00:41:20 -0700

    [330. Patching Array][Accepted]

--- 330-Patching-Array/solution.java
+++ 330-Patching-Array/solution.java
@@ -1,6 +1,7 @@
 public class Solution {
     public int minPatches(int[] nums, int n) {
-        int miss = 1, added = 0, i = 0;
+        long miss = 1;
+        int added = 0, i = 0;
         while(miss <= n) {
             if (i < nums.length && nums[i] <= miss) {
                 miss += nums[i];
