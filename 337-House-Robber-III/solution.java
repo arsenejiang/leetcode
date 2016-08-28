aa5011076526144402ea8e3a4c2b278cbe16238d
arsenejiang <wjiang@outlook.com>
2016-08-28 14:51:17 -0700

    [337. House Robber III][Accepted]

--- 337-House-Robber-III/solution.java
+++ 337-House-Robber-III/solution.java
@@ -19,8 +19,8 @@ public class Solution {
             return res;
         }
         
-        int left = helper(root.left);
-        int right = helper(root.right);
+        int[] left = helper(root.left);
+        int[] right = helper(root.right);
         
         res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]); // not rob root
         res[1] = root.val + left[0] + right[0]; // rob the root
