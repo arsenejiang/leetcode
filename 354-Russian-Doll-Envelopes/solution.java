9e58e2dafa33594409a2412c1e95367db164c757
arsenejiang <wjiang@outlook.com>
2016-08-28 21:09:51 -0700

    [354. Russian Doll Envelopes][Accepted]

---
+++ 354-Russian-Doll-Envelopes/solution.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes == null || envelopes.length == 0 || envelopes[0].length == 0) {
+            return 0;
+        }
+        
+        Comparator<int[]> comp = new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int b[]) {
+                if (a[0] != b[0]) {
+                    return a[0] - b[0];
+                }
+                
+                return b[1] - a[1];
+            }
+        };
+        
+        Arrays.sort(envelopes, comp);
+        int len = 0;
+        int[] dp = new int[envelopes.length];
+        for(int[] envelop : envelopes) {
+            int index = Arrays.binarySearch(dp, 0, len, envelop[1]);
+            if (index < 0) {
+                index = - index - 1;
+            }
+            dp[index] = envelop[1];
+            if (index == len) {
+                len++;
+            }
+        }
+        
+        return len;
+    }
+}
