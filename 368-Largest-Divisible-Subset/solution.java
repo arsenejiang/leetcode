b259d9a3b3bc0f3bcf9dc8b116c85b6d790bc867
arsenejiang <wjiang@outlook.com>
2016-08-29 23:02:05 -0700

    [368. Largest Divisible Subset][Runtime Error]

--- 368-Largest-Divisible-Subset/solution.java
+++ 368-Largest-Divisible-Subset/solution.java
@@ -7,45 +7,30 @@ public class Solution {
         
         Arrays.sort(nums);
         
-        List<List<Integer>> sets = new ArrayList();
-        for(int i = 0; i < nums.length; i++) {
-            helper(nums, i, sets);
-        }
-        
-        int maxSize = 0;
-        for(List<Integer> list : sets) {
-            if (list.size() > maxSize) {
-                maxSize = list.size();
-                res = list;
-            }
-        }
-        return res;
-    }
-    
-    private void helper(int[] nums, int index, List<List<Integer>> set) {
-        int size = set.size();
-        for(int i = 0; i < size; i++) {
-            List<Integer> list = set.get(i);
-            boolean valid = true;
-            for(int j = list.size() - 1; j >= 0; j--) {
-                if (!isValidPair(nums[index], list.get(j))) {
-                    valid = false;
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxIndex = 0, max = 1;
+        for(int i = 1; i < nums.length; i++) {
+            for(int j = i - 1; j >= 0; j--) {
+                if (nums[i] % nums[j] == 0) {
+                    dp[i] = dp[j] + 1;
+                    if (dp[i] > max) {
+                        max = dp[i];
+                        maxIndex = i;
+                    }
                     break;
                 }
             }
-            
-            if (valid) {
-                set.add(new ArrayList(list));
-                list.add(nums[index]);
+        }
+        
+        res.add(nums[maxIndex]);
+        for(int i = maxIndex - 1, index = maxIndex; i >= 0; i--) {
+            if (nums[maxIndex] % nums[i] == 0) {
+                res.add(nums[i]);
+                index = i;
             }
         }
         
-        List<Integer> emptyList = new ArrayList();
-        emptyList.add(nums[index]);
-        set.add(emptyList);
-    }
-    
-    private boolean isValidPair(int a, int b) {
-        return a % b == 0;
+        return res;
     }
 }
