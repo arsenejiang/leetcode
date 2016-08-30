02912314274971e6b0b9e294f3865960f507aa93
arsenejiang <wjiang@outlook.com>
2016-08-29 23:10:33 -0700

    [368. Largest Divisible Subset][Compile Error]

--- 368-Largest-Divisible-Subset/solution.java
+++ 368-Largest-Divisible-Subset/solution.java
@@ -9,23 +9,23 @@ public class Solution {
         
         int[] dp = new int[nums.length];
         dp[0] = 1;
-        int maxIndex = 0, max = 1;
         for(int i = 1; i < nums.length; i++) {
             for(int j = i - 1; j >= 0; j--) {
                 if (nums[i] % nums[j] == 0) {
-                    dp[i] = dp[j] + 1;
-                    if (dp[i] > max) {
-                        max = dp[i];
-                        maxIndex = i;
-                    }
-                    break;
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
         }
         
-        res.add(nums[maxIndex]);
-        for(int i = maxIndex - 1, index = maxIndex; i >= 0; i--) {
-            if (nums[maxIndex] % nums[i] == 0) {
+        int maxIndex = 0;
+        for(int i = 1; i < nums.length; i++) {
+            if (dp[i] > dp[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        
+        for(int i = maxIndex, index = maxIndex; i >= 0; i--) {
+            if (nums[index] % nums[i] == 0 && dp[index] = dp[i] + 1) {
                 res.add(nums[i]);
                 index = i;
             }
