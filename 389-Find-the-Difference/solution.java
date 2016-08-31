bfbdfd5423f55196d86d00e7dc61f229404acfde
arsenejiang <wjiang@outlook.com>
2016-08-30 23:29:45 -0700

    [389. Find the Difference][Compile Error]

---
+++ 389-Find-the-Difference/solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] scounts = new int[26];
+        for(char c : s.toCharArray()) {
+            scounts[c - 'a']++;
+        }
+        
+        char res;
+        for(char c : t.toCharArray()) {
+            if (scounts[c - 'a'] == 0) {
+                res = c;
+                break;
+            }
+            
+            scounts[c - 'a']--;
+        }
+        
+        return res;
+    }
+}
