1e467f33e6152a176828667d1ecd42efaf1a8d5e
arsenejiang <wjiang@outlook.com>
2016-08-30 23:33:19 -0700

    [389. Find the Difference][Accepted]

--- 389-Find-the-Difference/solution.java
+++ 389-Find-the-Difference/solution.java
@@ -1,4 +1,6 @@
 public class Solution {
+    /*
+    // array solution (6ms)
     public char findTheDifference(String s, String t) {
         int[] scounts = new int[26];
         for(char c : s.toCharArray()) {
@@ -12,5 +14,19 @@ public class Solution {
         }
         
         return 0;
+    }
+    */
+    
+    public char findTheDifference(String s, String t) {
+        char res = 0;
+        for(char c : s.toCharArray()) {
+            res ^= c;
+        }
+        
+        for(char c : t.toCharArray()) {
+            res ^= c;
+        }
+        
+        return res;
     }
 }
