0956ebd81c46d804b7d36bea618b12757381cafe
arsenejiang <wjiang@outlook.com>
2016-08-30 22:15:08 -0700

    [376. Wiggle Subsequence][Compile Error]

---
+++ 376-Wiggle-Subsequence/solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int wiggleMaxLength(int[] nums) {
+        if (nums == null) {
+            return 0;
+        }
+        
+        int len = nums.length;
+        if (len <= 1) {
+            return len;
+        }
+        
+        for(int i = 0; i < len; i++) {
+            if (i == 0) {
+                if (nums[0] != nums[1]) {
+                    res++;
+                }
+            }
+            else if (i == len - 1) {
+                if (nums[len - 2] != nums[len -1]) {
+                    res++;
+                }
+            }
+            else if ((nums[i] > nums[i-1] && nums[i] > nums[i+1]) || (nums[i] < nums[i-1] && nums[i] < nums[i+1])) {
+                res++;
+            }
+        }
+        
+        return res;
+    }
+}
