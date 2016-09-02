a36a7f8a0cfe3528b819013389174e8703bae042
arsenejiang <wjiang@outlook.com>
2016-09-02 15:21:40 -0700

    [208. Implement Trie (Prefix Tree)][Compile Error]

--- 208-Implement-Trie-(Prefix-Tree)/solution.java
+++ 208-Implement-Trie-(Prefix-Tree)/solution.java
@@ -1,3 +1,5 @@
+/*
+// lowercase letters a-z
 class TrieNode {
     // Initialize your data structure here.
     TrieNode[] children;
@@ -58,6 +60,68 @@ public class Trie {
             else {
                 cur = cur.children[c-'a'];
             }
+        }
+        
+        return true;
+    }
+}
+*/
+
+class TrieNode {
+    // Initialize your data structure here.
+    HashMap<Character, TrieNode> children;
+    boolean isLeaf;
+    public TrieNode() {
+        children = new HashMap<Character, TrieNode>();
+        isLeaf = false;
+    }
+}
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    // Inserts a word into the trie.
+    public void insert(String word) {
+        TrieNode cur = root;
+        for(int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i)；
+            if(!cur.children.containsKey(c)) {
+                cur.children.put(c, new TrieNode());
+            }
+            cur = cur.children.get(c);
+        }
+        
+        cur.isLeaf = true;
+    }
+
+    // Returns if the word is in the trie.
+    public boolean search(String word) {
+        TrieNode cur = root;
+        for(int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (!cur.children.containsKey(c)) {
+                return false;
+            }
+            cur = cur.children.get(c);
+        }
+        
+        return cur.isLeaf;
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix.
+    public boolean startsWith(String prefix) {
+        TrieNode cur = root;
+        for(int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (!cur.children.containsKey(c)) {
+                return false;
+            }
+            cur = cur.children.get(c);
         }
         
         return true;
