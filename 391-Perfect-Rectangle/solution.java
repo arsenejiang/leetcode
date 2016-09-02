ddda184b87dfb4de04e784595d5d9a37009fbeea
arsenejiang <wjiang@outlook.com>
2016-09-01 23:25:18 -0700

    [391. Perfect Rectangle][Accepted]

--- 391-Perfect-Rectangle/solution.java
+++ 391-Perfect-Rectangle/solution.java
@@ -1,43 +1,52 @@
 public class Solution {
     public boolean isRectangleCover(int[][] rectangles) {
-        if (rectangles == null || rectangles.length == 0 || rectangles[0].length == 0) {
-            return false;
-        }
-        
-        long areaSum = 0;
-        int minX = Integer.MAX_VALUE;
-        int minY = Integer.MAX_VALUE;
-        int maxX = Integer.MIN_VALUE;
-        int maxY = Integer.MIN_VALUE;
-        HashSet<String> map = new HashSet<String>();
-        for(int[] rect : rectangles) {
-            areaSum += (rect[3] - rect[1]) * (rect[2] - rect[0]);
-            minX = Math.min(minX, rect[0]);
-            minY = Math.min(minY, rect[1]);
-            maxX = Math.max(maxX, rect[2]);
-            maxY = Math.max(maxY, rect[3]);
+            if (rectangles.length == 0 || rectangles[0].length == 0) return false;
+
+            int x1 = Integer.MAX_VALUE;
+            int x2 = Integer.MIN_VALUE;
+            int y1 = Integer.MAX_VALUE;
+            int y2 = Integer.MIN_VALUE;
             
-            String s1 = rect[0] + " " + rect[1];
-            String s2 = rect[2] + " " + rect[3];
-            if (set.contains(s1)) {
-                set.remove(s1);
-            }
-            else {
-                set.add(s1);
+            HashSet<String> set = new HashSet<String>();
+            int area = 0;
+            
+            for (int[] rect : rectangles) {
+                x1 = Math.min(rect[0], x1);
+                y1 = Math.min(rect[1], y1);
+                x2 = Math.max(rect[2], x2);
+                y2 = Math.max(rect[3], y2);
+                
+                area += (rect[2] - rect[0]) * (rect[3] - rect[1]);
+                
+                String s1 = rect[0] + " " + rect[1];
+                String s2 = rect[0] + " " + rect[3];
+                String s3 = rect[2] + " " + rect[3];
+                String s4 = rect[2] + " " + rect[1];
+                
+                if (set.contains(s1)) {
+                    set.remove(s1);
+                } else {
+                    set.add(s1);
+                }
+                if (set.contains(s2)) {
+                    set.remove(s2);
+                } else {
+                    set.add(s2);
+                }
+                if (set.contains(s3)) {
+                    set.remove(s3);
+                } else {
+                    set.add(s3);
+                }
+                if (set.contains(s4)) {
+                    set.remove(s4);
+                } else {
+                    set.add(s4);
+                }
             }
             
-            if (set.contains(s2)) {
-                set.remove(s2);
-            }
-            else {
-                set.add(s2);
-            }
-        }
-        
-        if (!set.contains(minX + " " + minY) || !set.contains(minx + " " + maxY) || !set.contains(maxX + " " + minY) || !set.contains(maxX + " " + maxY)) {
-            return false;
-        }
-        
-        return areaSum == (maxY - minY) * (maxX - minX);
+            if (!set.contains(x1 + " " + y1) || !set.contains(x1 + " " + y2) || !set.contains(x2 + " " + y1) || !set.contains(x2 + " " + y2) || set.size() != 4) return false;
+            
+            return area == (x2-x1) * (y2-y1);
     }
 }
