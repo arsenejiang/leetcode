5fe580108209d79ccd71909a0d12370ecd3e6abd
arsenejiang <wjiang@outlook.com>
2016-09-02 00:10:25 -0700

    [23. Merge k Sorted Lists][Time Limit Exceeded]

---
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        
+        Comparator<ListNode> comp = new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode a, ListNode b) {
+                return a.val - b.val;
+            }
+        };
+        
+        PriorityQueue<ListNode> pq = new PriorityQueue(comp);
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        for(ListNode n : lists) {
+            pq.offer(n);
+        }
+        
+        while(!pq.isEmpty()) {
+            ListNode n = pq.poll();
+            cur.next = n;
+            cur = cur.next;
+            if (n.next != null) {
+                pq.offer(n.next);
+            }
+        }
+        
+        return dummy.next;
+    }
+}
