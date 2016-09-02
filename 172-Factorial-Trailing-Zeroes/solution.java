1bc0af65f17e31daa4b6dfc3e9c372adfa53f690
arsenejiang <wjiang@outlook.com>
2016-09-02 00:17:29 -0700

    [172. Factorial Trailing Zeroes][Accepted]

--- 172-Factorial-Trailing-Zeroes/solution.java
+++ 172-Factorial-Trailing-Zeroes/solution.java
@@ -1,7 +1,11 @@
 public class Solution {
     public int trailingZeroes(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        
         int result = 0;
-        while(n / 5 > 0) {
+        while(n >= 5) {
             result += n / 5;
             n = n / 5;
         }
