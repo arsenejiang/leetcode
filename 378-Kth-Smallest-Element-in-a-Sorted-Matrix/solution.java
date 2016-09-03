a26e386a2b406c955e2c83e506f09b61b742d029
arsenejiang <wjiang@outlook.com>
2016-09-03 15:40:28 -0700

    [378. Kth Smallest Element in a Sorted Matrix][Accepted]

--- 378-Kth-Smallest-Element-in-a-Sorted-Matrix/solution.java
+++ 378-Kth-Smallest-Element-in-a-Sorted-Matrix/solution.java
@@ -1,59 +1,41 @@
 public class Solution {
-    /*
     public int kthSmallest(int[][] matrix, int k) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0 || k <= 0) {
-            return 0;
-        }
+        int m = matrix.length;
+        int n = matrix[0].length;
         
-        int n = Math.min(k, matrix.length);
-        Comparator<int[]> comp = new Comparator<int[]>() {
+        Comparator<Point> comp = new Comparator<Point>() {
             @Override
-            public int compare(int[] a, int b[]) {
-                return matrix[a[0]][a[1]] - matrix[b[0]][b[1]];
-            }
+            public int compare(Point a, Point b) {
+                return matrix[a.x][a.y] - matrix[b.x][b.y];
+            }  
         };
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(k, comp);
+        PriorityQueue<Point> pq = new PriorityQueue<Point>(comp);
         
+        m = Math.min(k, m);
+        n = Math.min(k, n);
         for(int i = 0; i < n; i++) {
-            pq.offer(new int[]{0, i});
+            pq.offer(new Point(0, i));
         }
         
-        int size = 0;
-        int[] arr = new int[2];
-        while(k-- > 0 && !pq.isEmpty()) {
-            arr = pq.poll();
-            if (arr[0] < n - 1) {
-                pq.offer(new int[]{arr[0] + 1, arr[1]});
+        k = k - 1;
+        while(k-- > 0) {
+            Point p = pq.poll();
+            if (p.x <= m - 2) {
+                pq.offer(new Point(p.x + 1, p.y));
             }
         }
         
-        return matrix[arr[0]][arr[1]];
+        Point p = pq.peek();
+        return matrix[p.x][p.y]; 
+        
     }
-    */
     
-     public int kthSmallest(int[][] matrix, int k) {
-        int n = matrix.length;
-        PriorityQueue<Tuple> pq = new PriorityQueue<Tuple>();
-        for(int j = 0; j <= n-1; j++) pq.offer(new Tuple(0, j, matrix[0][j]));
-        for(int i = 0; i < k-1; i++) {
-            Tuple t = pq.poll();
-            if(t.x == n-1) continue;
-            pq.offer(new Tuple(t.x+1, t.y, matrix[t.x+1][t.y]));
-        }
-        return pq.poll().val;
-    }
-
-    class Tuple implements Comparable<Tuple> {
-        int x, y, val;
-        public Tuple (int x, int y, int val) {
+    class Point {
+        int x;
+        int y;
+        Point(int x, int y) {
             this.x = x;
             this.y = y;
-            this.val = val;
-        }
-        
-        @Override
-        public int compareTo (Tuple that) {
-            return this.val - that.val;
         }
     }
 }
