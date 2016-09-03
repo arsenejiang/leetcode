35412b43d5633b68450450d2657ba48c348f5ad7
arsenejiang <wjiang@outlook.com>
2016-09-02 22:03:02 -0700

    [9. Palindrome Number][Accepted]

---
+++ 9-Palindrome-Number/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        
+        if (x < 10) {
+            return true;
+        }
+        
+        if (x % 10 == 0) {
+            return false;
+        }
+        
+        int reverse = 0;
+        while(x > reverse) {
+            reverse = reverse * 10 + x % 10;
+            x = x / 10;
+        }
+        
+        return x == reverse || x == reverse / 10;
+    }
+}
