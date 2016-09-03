7fd660bcdf5063a3457a95b7b72a522c98e79aae
arsenejiang <wjiang@outlook.com>
2016-09-03 16:56:16 -0700

    [312. Burst Balloons][Accepted]

--- 312-Burst-Balloons/solution.java
+++ 312-Burst-Balloons/solution.java
@@ -4,27 +4,28 @@ public class Solution {
             return 0;
         }
         
-        int len = nums.length;
-        int[] arr = new int[len + 2];
-        for(int i = 1; i <= len; i++) {
+        int[] arr = new int[nums.length + 2];
+        int n = arr.length;
+        arr[0] = arr[n - 1] = 1;
+        for(int i = 1; i < n - 1; i++) {
             arr[i] = nums[i-1];
         }
-        arr[0] = 1;
-        arr[len + 1] = 1;
         
-        int[][] dp = new int[len  + 2][len + 2];
-        return helper(dp, arr, 1, len);
+        int[][] dp = new int[n][n];
+        return helper(arr, 0, n - 1, dp);
     }
     
-    private int helper(int[][] dp, int[] arr, int l, int r) {
-        if (dp[l][r] > 0) {
-            return dp[l][r];
+    private int helper(int[] arr, int left, int right, int[][] dp) {
+        if (dp[left][right] > 0) {
+            return dp[left][right];
         }
         
-        for(int i = l; i <= r; i++) {
-            dp[l][r] = Math.max(dp[l][r], helper(dp, arr, l, i - 1) + arr[l-1]*arr[i]*arr[r+1] + helper(dp, arr, i + 1, r));
+        int res = 0;
+        for(int i = left + 1; i <= right - 1; i++) {
+            res = Math.max(res, arr[left]*arr[right]*arr[i] + helper(arr, left, i, dp) + helper(arr, i, right, dp));
         }
         
-        return dp[l][r];
+        dp[left][right] = res;
+        return res;
     }
 }
