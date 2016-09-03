a9f0dc2716239803864bc261de9a75659583500d
arsenejiang <wjiang@outlook.com>
2016-09-03 12:32:15 -0700

    [235. Lowest Common Ancestor of a Binary Search Tree][Accepted]

--- 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/solution.java
+++ 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/solution.java
@@ -9,6 +9,10 @@
  */
 public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        
         if (p.val < root.val && q.val < root.val) {
             return lowestCommonAncestor(root.left, p, q);
         }
