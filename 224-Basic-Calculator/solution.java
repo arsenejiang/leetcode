b768ed8eeb026facc36fa9d95a1f318c14ca72e9
arsenejiang <wjiang@outlook.com>
2016-09-03 20:25:50 -0700

    [224. Basic Calculator][Compile Error]

--- 224-Basic-Calculator/solution.java
+++ 224-Basic-Calculator/solution.java
@@ -1,40 +1,35 @@
 public class Solution {
     public int calculate(String s) {
         Stack<Integer> stack = new Stack<Integer>();
-        int result = 0;
-        int num = 0;
         int sign = 1;
-        for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (Character.isDigit(c)) {
-                num = num * 10 + c - '0';
+        int result = 0;
+        int len = s.length();
+        for(int i = 0; i < len; i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                int num = s.charAt(i) - '0';
+                while(i + 1 < len && Character.isDigit(s.charAt(i+1))) {
+                    num = num * 10 + s.charAt(i + 1) - '0';
+                    i++;
+                }
+                result += num * sign;
             }
             else if (c == '+') {
-                result += sign * num;
                 sign = 1;
-                num = 0;
             }
             else if (c == '-') {
-                result += sign * num;
                 sign = -1;
-                num = 0;
             }
             else if (c == '(') {
                 stack.push(result);
                 stack.push(sign);
                 result = 0;
                 sign = 1;
-                num = 0;
             }
             else if (c == ')') {
-                result += sign * num;
-                num = 0;
-                result *= stack.pop();
-                result += stack.pop();
+                result = result * stack.pop() + stack.pop();
             }
         }
         
-        if(num != 0) result += sign * num;
-        return result;
+        
     }
 }
