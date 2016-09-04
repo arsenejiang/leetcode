7c852301a39c1df1eae4af869b17c28213834478
arsenejiang <wjiang@outlook.com>
2016-09-04 15:25:27 -0700

    [67. Add Binary][Accepted]

--- 67-Add-Binary/solution.java
+++ 67-Add-Binary/solution.java
@@ -1,25 +1,34 @@
 public class Solution {
     public String addBinary(String a, String b) {
+        if (a == null || a.isEmpty()) {
+            return b;
+        }
+        else if (b == null || b.isEmpty()) {
+            return a;
+        }
+        
         StringBuilder sb = new StringBuilder();
-        int aLen = a.length();
-        int bLen = b.length();
-        int i = aLen - 1;
-        int j = bLen - 1;
+        int m = a.length();
+        int n = b.length();
         int carry = 0;
+        int i = m - 1;
+        int j = n - 1;
         while(i >= 0 || j >= 0) {
-            int aVal = (i >= 0) ? a.charAt(i) - '0' : 0;
-            int bVal = (j >= 0) ? b.charAt(j) - '0' : 0;
-            int sum = aVal + bVal + carry;
-            sb.insert(0, sum % 2);
-            carry = sum / 2;
+            int val = carry;
+            val += (i >= 0) ? a.charAt(i) - '0' : 0;
+            val += (j >= 0) ? b.charAt(j) - '0' : 0;
+            char c = (char)('0' + val % 2);
+            sb.append(c);
+            carry = val / 2;
             i--;
             j--;
         }
         
         if (carry > 0) {
-            sb.insert(0, '1');
+            char c = (char)('0' + carry);
+            sb.append(c);
         }
         
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
