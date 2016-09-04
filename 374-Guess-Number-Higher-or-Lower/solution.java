4f820327f62c13eaebdb07dc52811054ad16dd6a
arsenejiang <wjiang@outlook.com>
2016-09-03 18:27:49 -0700

    [374. Guess Number Higher or Lower][Accepted]

--- 374-Guess-Number-Higher-or-Lower/solution.java
+++ 374-Guess-Number-Higher-or-Lower/solution.java
@@ -5,22 +5,21 @@
 
 public class Solution extends GuessGame {
     public int guessNumber(int n) {
-        int low = 1;
-        int high = n;
-        while(low < high) {
-            int mid = low + (high - low) / 2;
-            int result = guess(mid);
-            if (result == 0) {
+        int left = 1;
+        int right = n;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if (guess(mid) == 0) {
                 return mid;
             }
-            else if (result == 1) {
-                low = mid + 1;
+            else if (guess(mid) == -1) {
+                right = mid - 1;
             }
             else {
-                high = mid - 1;
+                left = mid + 1;
             }
         }
         
-        return low;
+        return left;
     }
 }
