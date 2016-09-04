9e6813bf31d3210e57f9db28187c2d570f837c81
arsenejiang <wjiang@outlook.com>
2016-09-04 15:40:29 -0700

    [15. 3Sum][Accepted]

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -14,20 +14,15 @@ public class Solution {
             int j = i + 1;
             int k = len - 1;
             while(j < k) {
-                if (j > i + 1 && nums[j] == nums[j-1]) {
-                    j++;
-                    continue;
-                }
-                
-                if (k < len - 1 && nums[k] == nums[k+1]) {
-                    k--;
-                    continue;
-                }
-                
                 int sum = nums[i] + nums[j] + nums[k];
                 if (sum == 0) {
-                    Integer[] arr = new Integer[]{nums[i], nums[j], nums[k]};
-                    res.add(Arrays.asList(arr));
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    while(j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                    while(j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
                     j++;
                     k--;
                 }
