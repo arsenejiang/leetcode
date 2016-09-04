786d6be0388d592eec34835c7ceec6f5030ac7db
arsenejiang <wjiang@outlook.com>
2016-09-04 13:45:14 -0700

    [283. Move Zeroes][Accepted]

--- 283-Move-Zeroes/solution.java
+++ 283-Move-Zeroes/solution.java
@@ -1,18 +1,32 @@
 public class Solution {
     public void moveZeroes(int[] nums) {
-        if (nums == null || nums.length == 0) {
+        if (nums == null || nums.length <= 1) {
             return;
         }
         
         int len = nums.length;
-        for(int i = 0, index = 0; i < len; i++) {
+        int nonZeroIndex = 0;
+        for(int i = 0; i < len; i++) {
             if (nums[i] != 0) {
-                if (i != index) {
-                    nums[index] = nums[i];
-                    nums[i] = 0;
+                continue;
+            }
+            
+            for(int j = Math.max(i + 1, nonZeroIndex); j < len; j++) {
+                if (nums[j] != 0) {
+                    swap(nums, i, j);
+                    nonZeroIndex = j + 1;
+                    if (nonZeroIndex == len) {
+                        return;
+                    }
+                    break;
                 }
-                index++;
             }
         }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
