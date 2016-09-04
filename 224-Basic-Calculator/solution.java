c1444c4c39608157c0d3c6f59eb1b315039e4a24
arsenejiang <wjiang@outlook.com>
2016-09-03 20:13:50 -0700

    [224. Basic Calculator][Accepted]

--- 224-Basic-Calculator/solution.java
+++ 224-Basic-Calculator/solution.java
@@ -1,13 +1,9 @@
 public class Solution {
     public int calculate(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-        
+        Stack<Integer> stack = new Stack<Integer>();
         int result = 0;
         int num = 0;
         int sign = 1;
-        Stack<Integer> stack = new Stack();
         for(int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (Character.isDigit(c)) {
@@ -15,30 +11,30 @@ public class Solution {
             }
             else if (c == '+') {
                 result += sign * num;
-                num = 0;
                 sign = 1;
+                num = 0;
             }
             else if (c == '-') {
                 result += sign * num;
-                num = 0;
                 sign = -1;
+                num = 0;
             }
             else if (c == '(') {
                 stack.push(result);
                 stack.push(sign);
                 result = 0;
                 sign = 1;
+                num = 0;
             }
             else if (c == ')') {
                 result += sign * num;
-                result = result * stack.pop();
-                result += stack.pop();
                 num = 0;
-                sign = 1;
+                result *= stack.pop();
+                result += stack.pop();
             }
         }
         
-        result += sign * num;
+        if(num != 0) result += sign * num;
         return result;
     }
 }
