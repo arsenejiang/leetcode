c24cc5ab9adaf52adb76d5932ee46b5cc792beee
arsenejiang <wjiang@outlook.com>
2016-09-04 17:34:32 -0700

    [44. Wildcard Matching][Accepted]

--- 44-Wildcard-Matching/solution.java
+++ 44-Wildcard-Matching/solution.java
@@ -1,11 +1,11 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-        int sp = 0, pp = 0;
-        int starIndex = -1, match = 0;
         int sLen = s.length();
         int pLen = p.length();
+        int sp = 0, pp = 0, match = 0;
+        int starIndex = -1;
         while(sp < sLen) {
-            if (pp < pLen && (s.charAt(sp) == p.charAt(pp) || p.charAt(pp) == '?')) {
+            if (pp < pLen && (p.charAt(pp) == '?' || p.charAt(pp) == s.charAt(sp))) {
                 pp++;
                 sp++;
             }
@@ -29,5 +29,6 @@ public class Solution {
         }
         
         return pp == pLen;
+        
     }
 }
