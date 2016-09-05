e51b9cd31b2620b9b93824e6abd5a5659d75c57c
arsenejiang <wjiang@outlook.com>
2016-09-05 00:00:29 -0700

    [219. Contains Duplicate II][Accepted]

--- 219-Contains-Duplicate-II/solution.java
+++ 219-Contains-Duplicate-II/solution.java
@@ -1,23 +1,19 @@
 public class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        if(nums == null || nums.length <= 1 || k <= 0) {
+        if (nums == null || nums.length <= 1) {
             return false;
         }
         
-        int len = nums.length;
-        Map<Integer, Integer> map = new HashMap();
-        for(int i = 0; i < len; i++) {
-            if (map.containsKey(nums[i])) {
-                int j = map.get(nums[i]);
-                if (i - j <= k) {
-                    return true;
-                }
-                else {
-                    map.put(nums[i], i);
-                }
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                set.remove(nums[i-k-1]);
+            }
+            if (set.contains(nums[i])) {
+                return true;
             }
             else {
-                map.put(nums[i], i);
+                set.add(nums[i]);
             }
         }
         
