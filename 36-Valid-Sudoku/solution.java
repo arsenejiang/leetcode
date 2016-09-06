189ec3ecfe6104af93647ff9d6a15a41610d9bb8
arsenejiang <wjiang@outlook.com>
2016-09-05 22:06:28 -0700

    [36. Valid Sudoku][Accepted]

--- 36-Valid-Sudoku/solution.java
+++ 36-Valid-Sudoku/solution.java
@@ -5,8 +5,18 @@ public class Solution {
         }
         
         for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board.length; j++) {
-                if (board[i][j] != '.' && !isValid(board, i, j)) {
+            if(!isValid(board, i, i, 0, 8)) {
+                return false;
+            }
+            
+            if(!isValid(board, 0, 8, i, i)) {
+                return false;
+            }
+        }
+        
+        for(int i = 0; i < 9; i+= 3) {
+            for(int j = 0; j < 9; j+= 3) {
+                if (!isValid(board, i, i + 2, j, j + 2)) {
                     return false;
                 }
             }
@@ -15,37 +25,16 @@ public class Solution {
         return true;
     }
     
-    private boolean isValid(char[][] board, int x, int y) {
-        for(int i = 0; i < board.length; i++) {
-            if (i == x) {
-                continue;
-            }
-            
-            if (board[i][y] == board[x][y]) {
-                return false;
-            }
-        }
-        
-        for(int i = 0; i < board[0].length; i++) {
-            if (i == y) {
-                continue;
-            }
-            
-            if (board[x][i] == board[x][y]) {
-                return false;
-            }
-        }
-        
-        int xx = x / 3 * 3;
-        int yy = y / 3 * 3;
-        for(int i = xx; i < xx + 3; i++) {
-            for(int j = yy; j < yy + 3; j++) {
-                if (i == x && j == y) {
-                    continue;
-                }
-                
-                if (board[i][j] == board[x][y]) {
-                    return false;
+    private boolean isValid(char[][] board, int rowStart, int rowEnd, int colStart, int colEnd) {
+        boolean[] count = new boolean[9];
+        for(int i = rowStart; i <= rowEnd; i++) {
+            for(int j = colStart; j <= colEnd; j++) {
+                if (board[i][j] != '.') {
+                    if (count[board[i][j] - '1']) {
+                        return false;
+                    }
+                    
+                    count[board[i][j] - '1'] = true;
                 }
             }
         }
