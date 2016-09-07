463a9729e4de7790eb1cd0c4a73c29b539ee5fbb
arsenejiang <wjiang@outlook.com>
2016-09-06 21:20:02 -0700

    [393. UTF-8 Validation][Accepted]

--- 393-UTF-8-Validation/solution.java
+++ 393-UTF-8-Validation/solution.java
@@ -1,41 +1,29 @@
 public class Solution {
     public boolean validUtf8(int[] data) {
-        return helper(data, 0);
-    }
-    
-    private boolean helper(int[] data, int start) {
-        if (start == data.length) {
-            return true;
-        }
-        
-        int count = numberOfLeadingOnes(data[start]);
-        if (count == 0) {
-            return helper(data, start + 1);
-        }
-        else if (count == 1) {
-            return false;
-        }
-        else {
-            int i = 1;
-            while(i < count) {
-                if (start + i == data.length || (data[start + i] & 0x80) != 0x80) {
+        int count = 0;
+        for(int num : data) {
+            if (count == 0) {
+                if ((num >> 5) == 0b110) {
+                    count = 1;
+                }
+                else if ((num >> 4) == 0b1110) {
+                    count = 2;
+                }
+                else if ((num >> 3) == 0b11110) {
+                    count = 3;
+                }
+                else if ((num >> 7) != 0) {
                     return false;
                 }
-                i++;
             }
-            
-            return helper(data, start + count);
-        }
-    }
-    
-    private int numberOfLeadingOnes(int num) {
-        int count = 0;
-        int start = 0x80;
-        while((num & start) != 0) {
-            count++;
-            start = start >>> 1;
+            else {
+                if ((num >> 6) != 0b10) {
+                    return false;
+                }
+                count--;
+            }
         }
         
-        return count;
+        return count == 0;
     }
 }
