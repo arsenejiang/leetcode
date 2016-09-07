66b809761fb47a9ff1e0220d10f23b33c7bbd41c
arsenejiang <wjiang@outlook.com>
2016-09-06 21:44:45 -0700

    [376. Wiggle Subsequence][Accepted]

--- 376-Wiggle-Subsequence/solution.java
+++ 376-Wiggle-Subsequence/solution.java
@@ -1,7 +1,6 @@
 public class Solution {
-    /*
     public int wiggleMaxLength(int[] nums) {
-        if (nums == null) {
+        if (nums == null || nums.length == 0) {
             return 0;
         }
         
@@ -11,46 +10,16 @@ public class Solution {
         }
         
         int res = 1;
-        for(int i = 0, j = 1; i < len - 1; i = j, j++) {
-            if (nums[i] < nums[j]) {
-                res++;
-                while(j <= len - 2 && nums[j] <= nums[j+1]) {
-                    j++;
-                }
-            }
-            else if (nums[i] > nums[j]) {
-                res++;
-                while(j <= len - 2 && nums[j] >= nums[j+1]) {
-                    j++;
-                }
-            }
-        }
-        
-        return res;
-    }
-    */
-    
-    public int wiggleMaxLength(int[] nums) {
-        if (nums == null) {
-            return 0;
-        }
-        
-        int len = nums.length;
-        if (len <= 1) {
-            return len;
-        }
-        
-        int res = 1;
-        for(int i = 0; i < len - 1; i++) {
+        for(int i = 0; i <= len - 2; i++) {
             if (nums[i] < nums[i + 1]) {
                 res++;
-                while(i <= len - 3 && nums[i + 1] <= nums[i + 2]) {
+                while(i + 2 <= len - 1 && nums[i + 1] <= nums[i + 2]) {
                     i++;
                 }
             }
             else if (nums[i] > nums[i + 1]) {
                 res++;
-                while(i <= len - 3 && nums[i + 1] >= nums[i + 2]) {
+                while(i + 2 <= len - 1 && nums[i + 1] >= nums[i + 2]) {
                     i++;
                 }
             }
@@ -58,5 +27,4 @@ public class Solution {
         
         return res;
     }
-    
 }
