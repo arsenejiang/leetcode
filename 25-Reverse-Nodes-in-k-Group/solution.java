4d21990951d8deda75ff5d73b12deb5a0eb2b555
arsenejiang <wjiang@outlook.com>
2016-09-07 23:57:40 -0700

    [25. Reverse Nodes in k-Group][Runtime Error]

---
+++ 25-Reverse-Nodes-in-k-Group/solution.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || head.next == null || k <= 1) {
+            return head;
+        }
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = head;
+        ListNode tail = head;
+        for(int i = 1; i < k; i++) {
+            if (cur == null) {
+                return head;
+            }
+            else {
+                cur = cur.next;
+            }
+        }
+        
+        k = k - 1;
+        while(k-- > 0) {
+            cur = tail.next;
+            tail.next = cur.next;
+            cur.next = dummy.next;
+            dummy.next = cur;
+        }
+        
+        tail.next = reverseKGroup(tail.next, k);
+        return dummy.next;
+    }
+}
