4bc670a46f5f4cbb8d1f64bb58dc4770653f52d8
arsenejiang <wjiang@outlook.com>
2016-09-07 22:08:27 -0700

    [268. Missing Number][Accepted]

--- 268-Missing-Number/solution.java
+++ 268-Missing-Number/solution.java
@@ -1,10 +1,24 @@
 public class Solution {
     public int missingNumber(int[] nums) {
-        int result = 0;
-        for(int i = 0; i < nums.length; i++) {
-            result ^= nums[i] ^ ( i + 1);
+        int len = nums.length;
+        for(int i = 0; i < len; i++) {
+            while(nums[i] < len && nums[i] != i) {
+                swap(nums, i, nums[i]);
+            }
         }
         
-        return result;
+        for(int i = 0; i < len; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        
+        return len;
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
