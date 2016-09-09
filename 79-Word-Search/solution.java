20868540e78562b414a68b58439a18ad9f62ea58
arsenejiang <wjiang@outlook.com>
2016-09-08 23:38:47 -0700

    [79. Word Search][Accepted]

--- 79-Word-Search/solution.java
+++ 79-Word-Search/solution.java
@@ -1,19 +1,25 @@
 public class Solution {
     public boolean exist(char[][] board, String word) {
-        if (board == null || board.length == 0 || board[0].length == 0) {
-            return false;
-        }
-        
         if (word == null || word.length() == 0) {
             return true;
         }
         
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return false;
+        }    
+        
+        char[] arr = word.toCharArray();
         int m = board.length;
         int n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(exist(board, word, i, j, 0)) {
-                    return true;
+                if (board[i][j] == arr[0]) {
+                    visited[i][j] = true;
+                    if (helper(board, i, j, arr, 1, visited)) {
+                        return true;
+                    }
+                    visited[i][j] = false;
                 }
             }
         }
@@ -21,21 +27,26 @@ public class Solution {
         return false;
     }
     
-    private boolean exist(char[][] board, String word, int row, int col, int index) {
-        if (index == word.length()) {
+    private boolean helper(char[][] board, int x, int y, char[] arr, int index, boolean[][] visited) {
+        if (index == arr.length) {
             return true;
         }
         
-        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] != word.charAt(index)) {
-            return false;
+        int[][] neighbors = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        for(int[] neighbor : neighbors) {
+            int i = x + neighbor[0];
+            int j = y + neighbor[1];
+            if (i >= 0 && i < board.length && j >= 0 && j < board[0].length) {
+                if (!visited[i][j] && board[i][j] == arr[index]) {
+                    visited[i][j] = true;
+                    if (helper(board, i, j, arr, index + 1, visited)) {
+                        return true;
+                    }
+                    visited[i][j] = false;
+                }
+            }
         }
         
-        board[row][col] = '.';
-        boolean result = exist(board, word, row - 1, col, index + 1) || 
-                         exist(board, word, row + 1, col, index + 1) ||
-                         exist(board, word, row, col - 1, index + 1) ||
-                         exist(board, word, row, col + 1, index + 1);
-        board[row][col] = word.charAt(index);
-        return result;
+        return false;
     }
 }
