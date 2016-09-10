7834a59bcdc3d0f2c859fb95cdec250554cbf453
arsenejiang <wjiang@outlook.com>
2016-09-10 00:13:51 -0700

    [59. Spiral Matrix II][Accepted]

--- 59-Spiral-Matrix-II/solution.java
+++ 59-Spiral-Matrix-II/solution.java
@@ -1,24 +1,35 @@
 public class Solution {
     public int[][] generateMatrix(int n) {
         int[][] res = new int[n][n];
+        if (n <= 0) {
+            return res;
+        }
+        else if (n == 1) {
+            res[0][0] = 1;
+            return res;
+        }
+        
         int rowBegin = 0, rowEnd = n - 1, colBegin = 0, colEnd = n - 1;
         int cur = 1;
         while(rowBegin <= rowEnd && colBegin <= colEnd) {
             for(int i = colBegin; i <= colEnd; i++) {
                 res[rowBegin][i] = cur++;
             }
+            
             rowBegin++;
             for(int i = rowBegin; i <= rowEnd; i++) {
                 res[i][colEnd] = cur++;
             }
+            
             colEnd--;
-            if (rowBegin <= rowEnd) {
+            if (colEnd >= colBegin) {
                 for(int i = colEnd; i >= colBegin; i--) {
-                    res[rowEnd][i] = cur++;
+                    res[rowEnd][i] = cur++;                    
                 }
                 rowEnd--;
             }
-            if (colBegin <= colEnd) {
+            
+            if (rowEnd >= rowBegin) {
                 for(int i = rowEnd; i >= rowBegin; i--) {
                     res[i][colBegin] = cur++;
                 }
