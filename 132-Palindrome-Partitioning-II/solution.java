25fd976fdc44b81594b271e339fee81e25467175
arsenejiang <wjiang@outlook.com>
2016-09-10 15:10:26 -0700

    [132. Palindrome Partitioning II][Accepted]

--- 132-Palindrome-Partitioning-II/solution.java
+++ 132-Palindrome-Partitioning-II/solution.java
@@ -4,6 +4,32 @@ public class Solution {
             return 0;
         }
         
+        int len = s.length();
+        char[] arr = s.toCharArray();
+        int[] dp = new int[len + 1];
+        for(int i = 0; i <= len; i++) {
+            dp[i] = i - 1;
+        }
+        
+        for(int i = 0; i < len; i++) {
+            for(int j = 0; i - j >= 0 && i + j < len && s.charAt(i-j) == s.charAt(i+j); j++) {
+                dp[i + j + 1] = Math.min(dp[i + j + 1], dp[i - j] + 1);
+            }
+            
+            for(int j = 0; i - j >= 0 && i + j + 1 < len && s.charAt(i - j) == s.charAt(i + j + 1); j++) {
+                dp[i + j + 2] = Math.min(dp[i + j + 2], dp[i - j] + 1);
+            }
+        }
+        
+        return dp[len];
+        
+    }
+    /*
+    public int minCut(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        
         char[] arr = s.toCharArray();
         int[] dp = new int[s.length() + 1];
         dp[0] = -1;
@@ -37,4 +63,5 @@ public class Solution {
         
         return true;
     }
+    */
 }
