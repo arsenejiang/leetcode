dd7035ceb3a2595c57af193dab7d9410431259ab
arsenejiang <wjiang@outlook.com>
2016-09-09 22:17:41 -0700

    [229. Majority Element II][Accepted]

--- 229-Majority-Element-II/solution.java
+++ 229-Majority-Element-II/solution.java
@@ -1,28 +1,26 @@
 public class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> res = new ArrayList();
-        if (nums == null || nums.length == 0) {
-            return res;
-        }
-        
-        int candidateA = nums[0];
-        int countA = 1;
-        int candidateB = 0;
+        int countA = 0;
         int countB = 0;
-        for(int i = 1; i < nums.length; i++) {
-            if (nums[i] == candidateA) {
+        int a = 0, b = 0;
+        for(int num : nums) {
+            if (num != b && countA == 0) {
+                a = num;
+                countA++;
+                continue;
+            }
+            
+            if (num != a && countB == 0) {
+                b = num;
+                countB++;
+                continue;
+            }
+            
+            if (num == a) {
                 countA++;
             }
-            else if (nums[i] == candidateB) {
+            else if (num == b) {
                 countB++;
-            }
-            else if (countA == 0) {
-                candidateA = nums[i];
-                countA = 1;
-            }
-            else if (countB == 0) {
-                candidateB = nums[i];
-                countB = 1;
             }
             else {
                 countA--;
@@ -33,20 +31,21 @@ public class Solution {
         countA = 0;
         countB = 0;
         for(int num : nums) {
-            if (num == candidateA) {
+            if (num == a) {
                 countA++;
             }
-            else if (num == candidateB) {
+            else if (num == b) {
                 countB++;
             }
         }
         
+        List<Integer> res = new ArrayList<Integer>();
         if (countA > nums.length / 3) {
-            res.add(candidateA);
+            res.add(a);
         }
         
         if (countB > nums.length / 3) {
-            res.add(candidateB);
+            res.add(b);
         }
         
         return res;
