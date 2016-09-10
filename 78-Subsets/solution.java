95b4613a04c764e1aa9fc1de466a98fe1ee479d5
arsenejiang <wjiang@outlook.com>
2016-09-09 23:34:32 -0700

    [78. Subsets][Accepted]

--- 78-Subsets/solution.java
+++ 78-Subsets/solution.java
@@ -1,16 +1,17 @@
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList();
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> empty = new ArrayList<Integer>();
+        res.add(empty);
         if (nums == null || nums.length == 0) {
             return res;
         }
         
-        res.add(new ArrayList<Integer>());
-        for(int i = 0; i < nums.length; i++) {
-            int size = res.size();
-            for(int j = 0; j < size; j++) {
-                List<Integer> list = new ArrayList<Integer>(res.get(j));
-                list.add(nums[i]);
+        for(int num : nums) {
+            int curSize = res.size();
+            for(int i = 0; i < curSize; i++) {
+                List<Integer> list = new ArrayList<Integer>(res.get(i));
+                list.add(num);
                 res.add(list);
             }
         }
