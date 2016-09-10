658de00c9174fbbb49eead420cd6194e147c7261
arsenejiang <wjiang@outlook.com>
2016-09-10 15:39:31 -0700

    [329. Longest Increasing Path in a Matrix][Time Limit Exceeded]

--- 329-Longest-Increasing-Path-in-a-Matrix/solution.java
+++ 329-Longest-Increasing-Path-in-a-Matrix/solution.java
@@ -1,40 +1,29 @@
 public class Solution {
-    private static final int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}}; 
     public int longestIncreasingPath(int[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return 0;
-        }
+        }   
         
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int[][] cache = new int[m][n];
-        int result = 0;
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                result = Math.max(result, helper(matrix, m, n, i, j, cache));
+        int[] res = new int[1];
+        res[0] = 0;
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                helper(matrix, Integer.MIN_VALUE, i, j, 0, res);
             }
         }
         
-        return result;
+        return res[0];
     }
     
-    private int helper(int[][] matrix, int m, int n, int i, int j, int[][] cache) {
-        if (cache[i][j] != 0) {
-            return cache[i][j];
+    private void helper(int[][] matrix, int prev, int row, int col, int cur, int[] res) {
+        if (row < 0 || col < 0 || row >= matrix.length || col >= matrix[0].length || prev >= matrix[row][col]) {
+            return;
         }
         
-        int max = 1;
-        for(int[] dir : dirs) {
-            int x = i + dir[0];
-            int y = j + dir[1];
-            if (x < 0 || x >= m || y < 0 || y >= n || matrix[x][y] <= matrix[i][j]) {
-                continue;
-            }
-            int len = 1 + helper(matrix, m, n, x, y, cache);
-            max = Math.max(max, len);
-        }
-        
-        cache[i][j] = max;
-        return max;
+        res[0] = Math.max(res[0], cur + 1);
+        helper(matrix, matrix[row][col], row - 1, col, cur + 1, res);
+        helper(matrix, matrix[row][col], row + 1, col, cur + 1, res);
+        helper(matrix, matrix[row][col], row, col - 1, cur + 1, res);
+        helper(matrix, matrix[row][col], row, col + 1, cur + 1, res);
     }
 }
