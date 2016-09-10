4fec8c003a5d74e11b6919f7f987f1d570d7b18f
arsenejiang <wjiang@outlook.com>
2016-09-10 14:40:15 -0700

    [132. Palindrome Partitioning II][Accepted]

--- 132-Palindrome-Partitioning-II/solution.java
+++ 132-Palindrome-Partitioning-II/solution.java
@@ -1,25 +1,40 @@
 public class Solution {
     public int minCut(String s) {
-        if (s == null || s.length() <= 1) {
+        if (s == null || s.length() == 0) {
             return 0;
         }
         
-        int len = s.length();
-        int[] dp = new int[len + 1];
-        for(int i = 0; i < len + 1; i++) {
-            dp[i] = i - 1;
+        char[] arr = s.toCharArray();
+        int[] dp = new int[s.length() + 1];
+        dp[0] = -1;
+        for(int i = 0; i < s.length(); i++) {
+            int localMin = dp[i] + 1;
+            for(int j = 0; j < i; j++) {
+                if (isPalindrome(arr, j, i)) {
+                    localMin = Math.min(localMin, dp[j] + 1);
+                }
+            }
+            dp[i + 1] = localMin;
         }
         
-        for(int i = 0; i < len; i++) {
-            for(int j = 0; i - j >= 0 && i + j < len && s.charAt(i-j) == s.charAt(i+j); j++) {
-                dp[i + j + 1] = Math.min(dp[i + j + 1], dp[i - j] + 1);
+        return dp[s.length()];
+    }
+    
+    private boolean isPalindrome(char[] arr, int left, int right) {
+        if (left >= right) {
+            return true;
+        }
+        
+        while(left < right) {
+            if (arr[left] == arr[right]) {
+                left++;
+                right--;
             }
-            
-            for(int j = 0; i - j >= 0 && i + j + 1 < len && s.charAt(i-j) == s.charAt(i+1+j); j++) {
-                dp[i + j + 2] = Math.min(dp[i + j + 2], dp[i - j] + 1);
+            else {
+                return false;
             }
         }
         
-        return dp[len];
+        return true;
     }
 }
