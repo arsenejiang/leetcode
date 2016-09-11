c9cee9987902dd20bcc8ccc5ed31656c70e4acfb
arsenejiang <wjiang@outlook.com>
2016-09-10 21:22:59 -0700

    [127. Word Ladder][Time Limit Exceeded]

--- 127-Word-Ladder/solution.java
+++ 127-Word-Ladder/solution.java
@@ -1,38 +1,42 @@
 public class Solution {
     public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
-        Set<String> beginSet = new HashSet();
-        Set<String> endSet = new HashSet();
+        HashSet<String> beginSet = new HashSet<String>();
+        HashSet<String> endSet = new HashSet<String>();
         beginSet.add(beginWord);
         endSet.add(endWord);
         int len = 1;
-        HashSet<String> visited = new HashSet();
-        while(!beginSet.isEmpty() && !endSet.isEmpty()) {
+        while(!beginSet.isEmpty() || !endSet.isEmpty()) {
             if (beginSet.size() > endSet.size()) {
-                Set<String> set = beginSet;
+                HashSet<String> temp = beginSet;
                 beginSet = endSet;
-                endSet = set;
+                endSet = temp;
             }
             
-            Set<String> temp = new HashSet();
+            HashSet<String> temp = new HashSet<String>();
             for(String s : beginSet) {
                 char[] arr = s.toCharArray();
                 for(int i = 0; i < arr.length; i++) {
                     char old = arr[i];
                     for(char c = 'a'; c <= 'z'; c++) {
-                        arr[i] = c;
-                        String target = String.valueOf(arr);
-                        if (endSet.contains(target)) {
-                            return len + 1;
+                        if (c == old) {
+                            continue;
                         }
                         
-                        if (!visited.contains(target) && wordList.contains(target)) {
-                            temp.add(target);
-                            visited.add(target);
+                        arr[i] = c;
+                        String newString = new String(arr);
+                        if (wordList.contains(newString)) {
+                            if (endSet.contains(newString)) {
+                                return len + 1;
+                            }
+                            else {
+                                temp.add(newString);
+                            }
                         }
                     }
                     arr[i] = old;
                 }
             }
+            
             beginSet = temp;
             len++;
         }
