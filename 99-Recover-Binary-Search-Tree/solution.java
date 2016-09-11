c6220ad1e077b3aa651e7cbae968f610b2d93ce5
arsenejiang <wjiang@outlook.com>
2016-09-10 22:32:56 -0700

    [99. Recover Binary Search Tree][Accepted]

--- 99-Recover-Binary-Search-Tree/solution.java
+++ 99-Recover-Binary-Search-Tree/solution.java
@@ -8,44 +8,32 @@
  * }
  */
 public class Solution {
+    TreeNode first = null;
+    TreeNode second = null;
+    TreeNode prev = new TreeNode(Integer.MIN_VALUE);
+    
     public void recoverTree(TreeNode root) {
+        traverse(root);
+        int val = first.val;
+        first.val = second.val;
+        second.val = val;
+    }
+    
+    private void traverse(TreeNode root) {
         if (root == null) {
             return;
         }
         
-        TreeNode n1 = null, n2 = null;
-        Stack<TreeNode> stack = new Stack();
-        TreeNode cur = root;
-        TreeNode prev = null;
-        
-        while(cur != null || !stack.isEmpty()) {
-            while(cur != null) {
-                stack.add(cur);
-                cur = cur.left;
-            }
-            
-            cur = stack.pop();
-            if (prev == null) {
-                prev = cur;
-            }
-            else {
-                if (prev.val > cur.val) {
-                    if (n1 == null) {
-                        n1 = prev;
-                        n2 = cur;
-                    }
-                    else {
-                        n2 = cur;
-                    }
-                }
-
-                prev = cur;
-            }
-            cur = cur.right;
+        traverse(root.left);
+        if (first == null && prev.val > root.val) {
+            first = prev;
         }
         
-        int temp = n1.val;
-        n1.val = n2.val;
-        n2.val = temp;
+        if (first != null && prev.val > root.val) {
+            second = root;
+        }
+        
+        prev = root;
+        traverse(root.right);
     }
 }
