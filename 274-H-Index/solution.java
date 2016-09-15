444e5b7256f554aa837c3856bf36cab7078f5649
arsenejiang <wjiang@outlook.com>
2016-09-15 00:10:29 -0700

    [274. H-Index][Accepted]

--- 274-H-Index/solution.java
+++ 274-H-Index/solution.java
@@ -1,47 +1,28 @@
 public class Solution {
-    /* sort array
-    public int hIndex(int[] citations) {
-        if (citations == null || citations.length == 0) {
-            return 0;
-        }
-        
-        Arrays.sort(citations);
-        int result = 0;
-        for(int i = citations.length - 1; i >= 0; i--) {
-            int curH = Math.min(citations[i], citations.length - i);
-            if (curH > result) {
-                result = curH;
-            }
-        }
-        
-        return result;
-    }
-    */
-    
     public int hIndex(int[] citations) {
         if (citations == null || citations.length == 0) {
             return 0;
         }
         
         int len = citations.length;
-        int[] h = new int[len + 1];
-        for(int i = 0; i < len; i++) {
-            if (citations[i] >= len) {
-                h[len]++;
+        int[] counts = new int[len + 1];
+        for(int c : citations) {
+            if (c >= len) {
+                counts[len]++;
             }
             else {
-                h[citations[i]]++;
+                counts[c]++;
             }
         }
         
-        int result = 0;
+        int sum = 0;
         for(int i = len; i >= 0; i--) {
-            result += h[i];
-            if (result >= i) {
+            sum += counts[i];
+            if (sum >= i) {
                 return i;
             }
         }
         
         return 0;
-    } 
+    }
 }
