c7cdeec385e156a47fea545bb05277df03c7d4ca
arsenejiang <wjiang@outlook.com>
2016-09-14 22:15:35 -0700

    [257. Binary Tree Paths][Accepted]

--- 257-Binary-Tree-Paths/solution.java
+++ 257-Binary-Tree-Paths/solution.java
@@ -8,56 +8,30 @@
  * }
  */
 public class Solution {
-    /*
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> res = new ArrayList();
-        if (root == null) {
-            return res;
-        }
-        
-        if (root.left == null && root.right == null) {
-            res.add(root.val + "");
-            return res;
-        }
-        
-        if (root.left != null) {
-            List<String> left = binaryTreePaths(root.left);
-            for(String l : left) {
-                res.add(root.val + "->" + l);
-            }
-        }
-        
-        if (root.right != null) {
-            List<String> right = binaryTreePaths(root.right);
-            for(String r : right) {
-                res.add(root.val + "->" + r);
-            }
-        }
-        
-        return res;
-    }
-    */
-    
-    public List<String> binaryTreePaths(TreeNode root) {
-        List<String> res = new ArrayList();
-        if (root != null) {
-            helper(root, "", res);
-        }
-        
+        List<String> res = new ArrayList<String>();
+        helper(root, "", res);
         return res;
     }
     
     private void helper(TreeNode root, String path, List<String> res) {
+        if (root == null) {
+            return;
+        }
+        
+        String s = path.length() > 0 ? "->" + root.val : Integer.toString(root.val);
+        
         if (root.left == null && root.right == null) {
-            res.add(path + root.val);
+            res.add(path + s);
+            return;
         }
         
         if (root.left != null) {
-            helper(root.left, path + root.val + "->", res);
+            helper(root.left, path + s, res);
         }
         
         if (root.right != null) {
-            helper(root.right, path + root.val + "->", res);
+            helper(root.right, path + s, res);
         }
     }
 }
