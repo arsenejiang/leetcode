fb21e6f7075bd13a362c829f3519e14a96ea1f63
arsenejiang <wjiang@outlook.com>
2016-09-14 23:01:25 -0700

    [120. Triangle][Time Limit Exceeded]

--- 120-Triangle/solution.java
+++ 120-Triangle/solution.java
@@ -1,35 +1,13 @@
 public class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        if (triangle == null) {
-            return 0;
+        return helper(triangle, 0, 0);
+    }
+    
+    private int helper(List<List<Integer>> triangle, int row, int col) {
+        if (row == triangle.size() - 1) {
+            return triangle.get(row).get(col);
         }
         
-        int row = triangle.size();
-        if (row == 0) {
-            return 0;
-        }
-        
-        int[] sum = new int[row];
-        int result = Integer.MAX_VALUE;
-        for(int i = 0; i < row; i++) {
-            for(int j = i; j >= 0; j--) {
-                if (j == 0) {
-                    sum[j] = sum[j] + triangle.get(i).get(j);
-                }
-                else if (j == i) {
-                    sum[j] = sum[j - 1] + triangle.get(i).get(j);
-                }
-                else {
-                    sum[j] = Math.min(sum[j-1], sum[j]) + triangle.get(i).get(j);
-                }
-                
-                if (i == row - 1) 
-                {
-                    result = Math.min(result, sum[j]);
-                }
-            }
-        }
-        
-        return result;
+        return triangle.get(row).get(col) + Math.min(helper(triangle, row + 1, col), helper(triangle, row + 1, col + 1));
     }
 }
