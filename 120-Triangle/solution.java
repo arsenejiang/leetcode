3bd266d7955287187f49a21298e219f4dd369e1e
arsenejiang <wjiang@outlook.com>
2016-09-14 23:32:39 -0700

    [120. Triangle][Runtime Error]

--- 120-Triangle/solution.java
+++ 120-Triangle/solution.java
@@ -1,4 +1,5 @@
 public class Solution {
+    /* TLE
     public int minimumTotal(List<List<Integer>> triangle) {
         return helper(triangle, 0, 0);
     }
@@ -9,5 +10,32 @@ public class Solution {
         }
         
         return triangle.get(row).get(col) + Math.min(helper(triangle, row + 1, col), helper(triangle, row + 1, col + 1));
+    }
+    */
+    
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int rows = triangle.size();
+        int cols = triangle.get(rows - 1).size();
+        int[][] dp = new int[rows][cols];
+        int result = Integer.MAX_VALUE;
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if (i == 0) {
+                    dp[i][j] = triangle.get(i).get(j);
+                }
+                else if (j == 0) {
+                    dp[i][j] = triangle.get(i).get(j) + dp[i-1][j];
+                }
+                else {
+                    dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i-1][j], dp[i-1][j-1]);
+                }
+                
+                if (i == rows - 1) {
+                    result = Math.min(dp[i][j], result);
+                }
+            }
+        }
+        
+        return result;
     }
 }
