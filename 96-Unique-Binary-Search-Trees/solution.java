a3eaa647aa414c3457424f9757168e1b5b07ad3d
arsenejiang <wjiang@outlook.com>
2016-09-15 22:36:25 -0700

    [96. Unique Binary Search Trees][Accepted]

--- 96-Unique-Binary-Search-Trees/solution.java
+++ 96-Unique-Binary-Search-Trees/solution.java
@@ -24,7 +24,7 @@ public class Solution {
     
     // iterative
     public int numTrees(int n) {
-        int[] dp = new dp[n + 1];
+        int[] dp = new int[n + 1];
         dp[0] = 1;
         for(int i = 1; i <= n; i++) {
             for(int j = 0; j <= i - 1; j++) {
