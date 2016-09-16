fcf7d4e4d705b1649f3c08f42ad914a3f07da35e
arsenejiang <wjiang@outlook.com>
2016-09-15 22:25:08 -0700

    [96. Unique Binary Search Trees][Runtime Error]

--- 96-Unique-Binary-Search-Trees/solution.java
+++ 96-Unique-Binary-Search-Trees/solution.java
@@ -1,20 +1,23 @@
 public class Solution {
     public int numTrees(int n) {
-        if (n == 0) {
-            return 1;
-        }
-        else if (n == 1) {
-            return 1;
-        }
-        else if (n == 2) {
-            return 2;
+        int[] cache = new int[n + 1];
+        cache[0] = 1;
+        cache[1] = 1;
+        cache[2] = 2;
+        return helper(n, cache);
+    }
+    
+    private int helper(int n, int[] cache) {
+        if (cache[n] != 0) {
+            return cache[n];
         }
         
         int count = 0;
         for(int i = 1; i <= n; i++) {
-            count += numTrees(i - 1) * numTrees(n - i);
+            count += helper(i - 1, cache) * helper(n - i, cache);
         }
         
+        cache[n] = count;
         return count;
     }
 }
