5d9f45931f851768445556e1cb2b2f95eb552822
arsenejiang <wjiang@outlook.com>
2016-09-15 22:22:13 -0700

    [96. Unique Binary Search Trees][Time Limit Exceeded]

--- 96-Unique-Binary-Search-Trees/solution.java
+++ 96-Unique-Binary-Search-Trees/solution.java
@@ -1,15 +1,20 @@
 public class Solution {
     public int numTrees(int n) {
-        int[] dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
-        
-        for(int i = 2; i <= n; i++) {
-            for(int j = 0; j <= i - 1; j++) {
-                dp[i] += dp[j]*dp[i - j - 1];
-            }
+        if (n == 0) {
+            return 1;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+        else if (n == 2) {
+            return 2;
         }
         
-        return dp[n];
+        int count = 0;
+        for(int i = 1; i <= n; i++) {
+            count += numTrees(i - 1) * numTrees(n - i);
+        }
+        
+        return count;
     }
 }
