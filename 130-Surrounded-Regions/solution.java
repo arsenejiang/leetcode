43dc419c85b3b46a1bafd8d8000cd00278c614c2
arsenejiang <wjiang@outlook.com>
2016-09-17 09:32:14 -0700

    [130. Surrounded Regions][Runtime Error]

--- 130-Surrounded-Regions/solution.java
+++ 130-Surrounded-Regions/solution.java
@@ -1,19 +1,50 @@
 public class Solution {
     public void solve(char[][] board) {
-        if (board == null || board.length <= 1 || board[0].length <= 1) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
             return;
         }
         
         int m = board.length;
         int n = board[0].length;
+        Queue<int[]> q = new LinkedList<int[]>();
         for(int i = 0; i < m; i++) {
-            check(board, i, 0, m, n);
-            check(board, i, n - 1, m, n);
+            for(int j = 0; j < n; j++) {
+                if (i == 0 || i == m - 1) {
+                    if (board[i][j] == 'O') {
+                        board[i][j] = 'B';
+                        q.offer(new int[]{i, j});
+                    }
+                }
+                else if (j == 0 || j == n - 1) {
+                    if (board[i][j] == 'O') {
+                        board[i][j] = 'B';
+                        q.offer(new int[]{i, j});
+                    }
+                }
+            }
         }
         
-        for(int j = 0; j < n; j++) {
-            check(board, 0, j, m, n);
-            check(board, m - 1, j, m, n);
+        while(!q.isEmpty()) {
+            int[] p = q.poll();
+            if (p[0] >= 1 && board[p[0] - 1][p[1]] == 'O') {
+                board[p[0] - 1][p[1]] = 'B';
+                q.offer(new int[]{p[0] - 1, p[1]});
+            }
+            
+            if (p[0] <= n - 2 && board[p[0] + 1][p[1]] == 'O') {
+                board[p[0] + 1][p[1]] = 'B';
+                q.offer(new int[]{p[0] + 1, p[1]});
+            }
+            
+            if (p[1] >= 1 && board[p[0]][p[1] - 1] == 'O') {
+                board[p[0]][p[1] - 1] = 'B';
+                q.offer(new int[]{p[0], p[1] - 1});
+            }
+            
+            if (p[1] <= m - 2 && board[p[0]][p[1] + 1] == 'O') {
+                board[p[0]][p[1] + 1] = 'B';
+                q.offer(new int[]{p[0], p[1] + 1});
+            }
         }
         
         for(int i = 0; i < m; i++) {
@@ -21,35 +52,9 @@ public class Solution {
                 if (board[i][j] == 'O') {
                     board[i][j] = 'X';
                 }
-            }
-        }
-        
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if (board[i][j] == 'B') {
+                else if (board[i][j] == 'B') {
                     board[i][j] = 'O';
                 }
-            }
-        }
-    }
-    
-    private void check(char[][] board, int i, int j, int m, int n) {
-        if(board[i][j] == 'O') {
-            board[i][j] = 'B';
-            if (i > 1) {
-                check(board, i - 1, j, m, n);
-            }
-            
-            if (i < m - 2) {
-                check(board, i + 1, j, m, n);
-            }
-            
-            if (j > 1) {
-                check(board, i, j - 1, m, n);
-            }
-            
-            if (j < n - 2) {
-                check(board, i, j + 1, m, n);
             }
         }
     }
