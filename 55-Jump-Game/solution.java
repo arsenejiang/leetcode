275c0d361c30ba61b95ed1427f7b15631f61803f
arsenejiang <wjiang@outlook.com>
2016-09-16 21:35:09 -0700

    [55. Jump Game][Accepted]

--- 55-Jump-Game/solution.java
+++ 55-Jump-Game/solution.java
@@ -1,4 +1,5 @@
 public class Solution {
+    /*
     public boolean canJump(int[] nums) {
         if (nums == null || nums.length <= 1) {
             return true;
@@ -25,5 +26,21 @@ public class Solution {
         }
         
         return false;
+    }
+    */
+    
+    public boolean canJump(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return true;
+        }
+        
+        int lastPos = nums.length - 1;
+        for(int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        
+        return lastPos == 0;
     }
 }
