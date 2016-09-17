984f2090db1e9529322489675b63cf77971eab2b
arsenejiang <wjiang@outlook.com>
2016-09-17 00:16:07 -0700

    [50. Pow(x, n)][Accepted]

--- 50-Pow(x,-n)/solution.java
+++ 50-Pow(x,-n)/solution.java
@@ -1,23 +1,23 @@
 public class Solution {
     public double myPow(double x, int n) {
+        return helper(x, n);
+    }
+    
+    private double helper(double x, long n) {
         if (n == 0) {
             return 1;
         }
-        else if (Double.compare(x, 1) == 0) {
-            return 1;
-        }
-        else if (n < 0) {
-            return 1 / myPow(x, -n);
-        }
-        else if (n == 1) {
-            return x;
+        
+        if (n < 0) {
+            n = -n;
+            x = 1 / x;
         }
         
         if (n % 2 == 0) {
-            return myPow(x * x, n / 2);
+            return helper(x * x, n / 2);
         }
         else {
-            return x * myPow(x * x, (n - 1) / 2);
+            return x * helper(x * x, (n - 1) / 2);
         }
     }
 }
