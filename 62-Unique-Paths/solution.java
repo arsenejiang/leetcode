a05558bd04b20850d501c5a140431755df8fd94a
arsenejiang <wjiang@outlook.com>
2016-09-18 12:39:48 -0700

    [62. Unique Paths][Accepted]

--- 62-Unique-Paths/solution.java
+++ 62-Unique-Paths/solution.java
@@ -23,9 +23,9 @@ public class Solution {
     
     // match solution
     public int uniquePaths(int m, int n) {
-        double res = 1;
-        for(int i = m - 1; i >= 1; i++) {
-            res *= (n - 1 + i) / i;
+        long res = 1;
+        for(int i = 1; i <= m - 1; i++) {
+            res = res * (n - 1 + i) / i;
         }
         
         return (int)res;
