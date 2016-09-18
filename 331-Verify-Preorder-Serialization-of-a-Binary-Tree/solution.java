01a1b4b66e36a3fb0d491052e93c587e465161ea
arsenejiang <wjiang@outlook.com>
2016-09-18 11:53:54 -0700

    [331. Verify Preorder Serialization of a Binary Tree][Time Limit Exceeded]

--- 331-Verify-Preorder-Serialization-of-a-Binary-Tree/solution.java
+++ 331-Verify-Preorder-Serialization-of-a-Binary-Tree/solution.java
@@ -1,17 +1,34 @@
 public class Solution {
     public boolean isValidSerialization(String preorder) {
-        String[] arr = preorder.split(",");
-        int diff = 1;
-        for(int i = 0; i < arr.length; i++) {
-            diff--;
-            if (diff < 0) {
-                return false;
+        if (preorder == null || preorder.length() == 0) {
+            return true;
+        }
+        
+        String[] strs = preorder.split(",");
+        
+        return isValid(strs, 0, strs.length - 1);
+    }
+    
+    private boolean isValid(String[] strs, int start, int end) {
+        if (start == end) {
+            if (strs[start].equals("#")) {
+                return true;
             }
-            if (!arr[i].equals("#")) {
-                diff += 2;
+            else {
+                return false;
             }
         }
         
-        return diff == 0;
+        if (strs[start].equals("#")) {
+            return false;
+        }
+        
+        for(int i = start + 1; i <= end - 1; i += 2) {
+            if (isValid(strs, start + 1, i) && isValid(strs, i + 1, end)) {
+                return true;
+            }
+        }
+        
+        return false;
     }
 }
