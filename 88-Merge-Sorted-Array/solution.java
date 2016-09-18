d8d87f147e0fa3f2a794f89fc440e88d240850ea
arsenejiang <wjiang@outlook.com>
2016-09-18 00:45:05 -0700

    [88. Merge Sorted Array][Runtime Error]

--- 88-Merge-Sorted-Array/solution.java
+++ 88-Merge-Sorted-Array/solution.java
@@ -1,21 +1,9 @@
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1;
-        int j = n - 1;
-        int index = m + n - 1;
-        while(i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[index--] = nums1[i--];
-            }
-            else {
-                nums1[index--] = nums2[j--];
-            }
-        }
-        
-        if (i < 0) {
-            while(j >= 0) {
-                nums1[index--] = nums2[j--];
-            }
+        int i1 = m - 1;
+        int i2 = n - 1;
+        for(int i = m + n - 1; i >= 0; i--) {
+            nums1[i] = (i2 < 0 || nums1[i1] >= nums2[i2]) ? nums1[i1--] : nums2[i2--];
         }
     }
 }
