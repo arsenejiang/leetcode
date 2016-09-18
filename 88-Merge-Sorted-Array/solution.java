300b638a41223fa4c80fb20eb5bf57979f3ef12b
arsenejiang <wjiang@outlook.com>
2016-09-18 00:50:17 -0700

    [88. Merge Sorted Array][Accepted]

--- 88-Merge-Sorted-Array/solution.java
+++ 88-Merge-Sorted-Array/solution.java
@@ -3,7 +3,7 @@ public class Solution {
         int i1 = m - 1;
         int i2 = n - 1;
         for(int i = m + n - 1; i >= 0; i--) {
-            nums1[i] = (i2 < 0 || nums1[i1] >= nums2[i2]) ? nums1[i1--] : nums2[i2--];
+            nums1[i] = (i2 < 0 || (i1 >= 0 && nums1[i1] >= nums2[i2])) ? nums1[i1--] : nums2[i2--];
         }
     }
 }
