3b2f50085fc1020e950ca25306d49c88fae62e7f
arsenejiang <wjiang@outlook.com>
2016-09-18 12:22:40 -0700

    [63. Unique Paths II][Runtime Error]

--- 63-Unique-Paths-II/solution.java
+++ 63-Unique-Paths-II/solution.java
@@ -5,25 +5,29 @@ public class Solution {
         }
         
         int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
+        int n = obstacleGrid.length;
         int[][] dp = new int[m][n];
-        for(int i = 0; i < m; i++) {
-            if(obstacleGrid[i][0] == 1) {
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        
+        dp[0][0] = 1;
+        
+        for(int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 1) {
                 dp[i][0] = 0;
-                break;
             }
             else {
-                dp[i][0] = 1;
+                dp[i][0] = dp[i-1][0];
             }
         }
         
-        for(int j = 0; j < n; j++) {
-            if (obstacleGrid[0][j] == 1) {
-                dp[0][j] = 0;
-                break;
+        for(int i = 1; i < n; i++) {
+            if (obstacleGrid[0][i] == 1) {
+                dp[0][i] = 0;
             }
             else {
-                dp[0][j] = 1;
+                dp[0][i] = dp[0][i-1];
             }
         }
         
