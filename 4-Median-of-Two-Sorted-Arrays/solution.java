6b5b38d6e070d69654cf3f1cfd746d1f0d937f01
arsenejiang <wjiang@outlook.com>
2016-09-19 01:21:30 -0700

    [4. Median of Two Sorted Arrays][Accepted]

--- 4-Median-of-Two-Sorted-Arrays/solution.java
+++ 4-Median-of-Two-Sorted-Arrays/solution.java
@@ -8,34 +8,32 @@ public class Solution {
         else {
             int k1 = findKth(nums1, nums2, 0, m - 1, 0, n - 1, (m + n) / 2);
             int k2 = findKth(nums1, nums2, 0, m - 1, 0, n - 1, (m + n) / 2 + 1);
-            return (double)(k1 + k2) / 2;
+            return (double)k1 / 2 + (double)k2 / 2;
         }
     }
     
     private int findKth(int[] nums1, int[] nums2, int start1, int end1, int start2, int end2, int k) {
-        if (start1 > end1) {
-            return nums2[start2 + k - 1];    
+        int m = end1 - start1 + 1;
+        int n = end2 - start2 + 1;
+        if (m > n) {
+            return findKth(nums2, nums1, start2, end2, start1, end1, k);
         }
-        else if (start2 > end2) {
-            return nums1[start1 + k - 1];
+        
+        if (m == 0) {
+            return nums2[k - 1];
         }
         
         if (k == 1) {
             return Math.min(nums1[start1], nums2[start2]);
         }
         
-        int total = end1 - start1 + 1 + end2 - start2 + 1;
-        if (k == total) {
-            return Math.max(nums1[end1], nums2[end2]);
-        }
-        
-        int m1 = Math.min(end1, start1 + k / 2);
-        int m2 = Math.min(end2, start2 + k / 2);
-        if (nums1[m1] < nums2[m2]) {
-            return findKth(nums1, nums2, m1 + 1, end1, start2, end2, k - (m1 - start1 + 1));
+        int part1 = Math.min(m, k / 2);
+        int part2 = k - part1;
+        if (nums1[start1 + part1 - 1] < nums2[start2 + part2 - 1]) {
+            return findKth(nums1, nums2, start1 + part1, end1, start2, end2, k - part1);
         }
         else {
-            return findKth(nums1, nums2, start1, end1, m2 + 1, end2, k - (m2 - start2 + 1));
+            return findKth(nums1, nums2, start1, end1, start2 + part2, end2, k - part2);
         }
     }
 }
