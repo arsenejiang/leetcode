b90baec1c88a529ae81750a0a926f02b827208fd
arsenejiang <wjiang@outlook.com>
2016-09-18 23:33:31 -0700

    [318. Maximum Product of Word Lengths][Accepted]

--- 318-Maximum-Product-of-Word-Lengths/solution.java
+++ 318-Maximum-Product-of-Word-Lengths/solution.java
@@ -1,17 +1,20 @@
 public class Solution {
-    
-    // bit manipulation
     public int maxProduct(String[] words) {
-        int[] nums = new int[words.length];
-        for(int i = 0; i < words.length; i++) {
+        if (words == null || words.length <= 1) {
+            return 0;
+        }
+        
+        int len = words.length;
+        int[] nums = new int[len];
+        for(int i = 0; i < len; i++) {
             for(int j = 0; j < words[i].length(); j++) {
-                nums[i] |= 1 << (words[i].charAt(j) - 'a');
+                char c = words[i].charAt(j);
+                nums[i] = nums[i] | (1 << (c - 'a'));
             }
         }
         
         int max = 0;
-        
-        for(int i = 0; i < words.length; i++) {
+        for(int i = 0; i < words.length - 1; i++) {
             for(int j = i + 1; j < words.length; j++) {
                 if ((nums[i] & nums[j]) == 0) {
                     max = Math.max(max, words[i].length() * words[j].length());
