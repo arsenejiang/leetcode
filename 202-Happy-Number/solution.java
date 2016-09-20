e4bdea98ef5b9421f827ded91b93d88688a112f6
arsenejiang <wjiang@outlook.com>
2016-09-19 22:55:55 -0700

    [202. Happy Number][Accepted]

--- 202-Happy-Number/solution.java
+++ 202-Happy-Number/solution.java
@@ -1,24 +1,29 @@
 public class Solution {
     public boolean isHappy(int n) {
-        Set<Integer> visited = new HashSet();
+        if (n <= 0) {
+            return false;
+        }
+        
+        HashSet<Integer> set = new HashSet<Integer>();
         while(n != 1) {
-            if (visited.contains(n)) {
+            int val = 0;
+            while(n != 0) {
+                val += (n % 10) * (n % 10);
+                n = n / 10;
+            }
+            
+            n = val;
+            if (n == 1) {
+                return true;
+            }
+            else if (set.contains(n)) {
                 return false;
             }
-            visited.add(n);
-            n = getNext(n);
+            else {
+                set.add(n);
+            }
         }
         
         return true;
-    }
-    
-    private int getNext(int n) {
-        int sum = 0;
-        while(n > 0) {
-            sum += (n % 10) * (n % 10);
-            n = n / 10;
-        }
-        
-        return sum;
     }
 }
