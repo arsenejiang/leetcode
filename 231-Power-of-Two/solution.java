8eff5d510737cd1a45b121b4cb23611a02089e29
arsenejiang <wjiang@outlook.com>
2016-09-19 22:33:13 -0700

    [231. Power of Two][Accepted]

--- 231-Power-of-Two/solution.java
+++ 231-Power-of-Two/solution.java
@@ -1,5 +1,9 @@
 public class Solution {
     public boolean isPowerOfTwo(int n) {
-        return n > 0 && ((n & (n - 1)) == 0);   
+        if (n <= 0) {
+            return false;
+        }
+        
+        return (n & (n - 1)) == 0;
     }
 }
