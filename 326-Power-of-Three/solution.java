acf26f0b3c53e6e17a2829faa378d5a6c6dd3c35
arsenejiang <wjiang@outlook.com>
2016-09-19 21:45:26 -0700

    [326. Power of Three][Accepted]

--- 326-Power-of-Three/solution.java
+++ 326-Power-of-Three/solution.java
@@ -1,20 +1,9 @@
 public class Solution {
-    // log
-    /*
     public boolean isPowerOfThree(int n) {
-        return (Math.log10(n) / Math.log10(3)) % 1 == 0;
-    }
-    */
-    
-    public boolean isPowerOfThree(int n) {
-        if (n < 1) {
+        if (n <= 0) {
             return false;
-        }
+        }    
         
-        while(n % 3 == 0) {
-            n = n / 3;
-        }
-        
-        return n == 1;
+        return (Math.log10(n) / Math.log10(3)) % 1 == 0;
     }
 }
