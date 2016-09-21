97824b679a88f457835d11566fe3699ab9628e5e
arsenejiang <wjiang@outlook.com>
2016-09-20 22:32:19 -0700

    [125. Valid Palindrome][Time Limit Exceeded]

--- 125-Valid-Palindrome/solution.java
+++ 125-Valid-Palindrome/solution.java
@@ -1,26 +1,15 @@
 public class Solution {
     public boolean isPalindrome(String s) {
-        if (s == null) {
+        if (s == null || s.length() <= 1) {
             return true;
         }
         
-        int len = s.length();
-        int left = 0, right = len - 1;
+        char[] arr = s.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
         while(left < right) {
-            if (!Character.isLetterOrDigit(s.charAt(left))) {
-                left++;
-            }
-            else if (!Character.isLetterOrDigit(s.charAt(right))) {
-                right--;
-            }
-            else {
-                if(Character.toLowerCase(s.charAt(left)) == Character.toLowerCase(s.charAt(right))) {
-                    left++;
-                    right--;
-                }
-                else {
-                    return false;
-                }
+            if (arr[left++] != arr[right--]) {
+                return false;
             }
         }
         
