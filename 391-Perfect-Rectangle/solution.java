0f3c803c768d1c08ea541f270c260e3d2a9d7a94
arsenejiang <wjiang@outlook.com>
2016-09-20 22:11:18 -0700

    [391. Perfect Rectangle][Accepted]

--- 391-Perfect-Rectangle/solution.java
+++ 391-Perfect-Rectangle/solution.java
@@ -1,26 +1,24 @@
 public class Solution {
     public boolean isRectangleCover(int[][] rectangles) {
         if (rectangles == null || rectangles.length == 0 || rectangles[0].length == 0) {
-            return false;
+            return true;
         }
         
-        int areaSum = 0;
         int x1 = Integer.MAX_VALUE;
         int y1 = Integer.MAX_VALUE;
         int x2 = Integer.MIN_VALUE;
         int y2 = Integer.MIN_VALUE;
+        int area = 0;
         HashSet<String> set = new HashSet<String>();
         for(int[] rect : rectangles) {
-            areaSum += (rect[3] - rect[1]) * (rect[2] - rect[0]);
-            x1 = Math.min(x1, rect[0]);
-            y1 = Math.min(y1, rect[1]);
-            x2 = Math.max(x2, rect[2]);
-            y2 = Math.max(y2, rect[3]);
+            x1 = Math.min(rect[0], x1);
+            y1 = Math.min(rect[1], y1);
+            x2 = Math.max(rect[2], x2);
+            y2 = Math.max(rect[3], y2);
             
-            String[] s = new String[] { rect[0] + " " + rect[1], rect[0] + " " + rect[3], 
-                rect[2] + " " + rect[1], rect[2] + " " + rect[3]};
-            
-            for(String str : s) {
+            area += (rect[3] - rect[1]) * (rect[2] - rect[0]);
+            String[] strs = new String[] {rect[0] + " " + rect[1], rect[0] + " " + rect[3], rect[2] + " " + rect[1], rect[2] + " " + rect[3]};
+            for(String str : strs) {
                 if (set.contains(str)) {
                     set.remove(str);
                 }
@@ -30,10 +28,11 @@ public class Solution {
             }
         }
         
-        if (!set.contains(x1 + " " + y1) || !set.contains(x2 + " " + y2) || !set.contains(x1 + " " + y2) || !set.contains(x2 + " " + y1) || set.size() != 4){
+        if (!set.contains(x1 + " " + y1) || !set.contains(x1 + " " + y2) || !set.contains(x2 + " " + y1) || !set.contains(x2 + " " + y2) || set.size() != 4) {
             return false;
         }
         
-        return areaSum == (y2 - y1) * (x2 - x1);
+        int totalArea = (y2 - y1) * (x2 - x1);
+        return totalArea == area;
     }
 }
