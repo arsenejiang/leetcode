6947b53c2e1287469ecb0e9ccc26e558bbac9650
arsenejiang <wjiang@outlook.com>
2016-09-24 15:58:03 -0700

    [349. Intersection of Two Arrays][Accepted]

--- 349-Intersection-of-Two-Arrays/solution.java
+++ 349-Intersection-of-Two-Arrays/solution.java
@@ -1,27 +1,21 @@
 public class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        List<Integer> res = new ArrayList();
-        if (nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0) {
-            return new int[0];
-        }
-        
-        Set<Integer> set = new HashSet<Integer>();
+        HashSet<Integer> res = new HashSet<Integer>();
+        HashSet<Integer> set = new HashSet<Integer>();
         for(int num : nums1) {
-            if (!set.contains(num)) {
-                set.add(num);
-            }
+            set.add(num);
         }
         
-        for(int num: nums2) {
+        for(int num : nums2) {
             if (set.contains(num)) {
-                set.remove(num);
                 res.add(num);
             }
         }
         
         int[] result = new int[res.size()];
-        for(int i = 0; i < result.length; i++) {
-            result[i] = res.get(i);
+        int i = 0;  
+        for(int num : res) {
+            result[i++] = num;
         }
         
         return result;
