315be541c06b516967d9133febac80ca34315949
arsenejiang <wjiang@outlook.com>
2016-09-23 22:31:08 -0700

    [92. Reverse Linked List II][Runtime Error]

--- 92-Reverse-Linked-List-II/solution.java
+++ 92-Reverse-Linked-List-II/solution.java
@@ -8,32 +8,27 @@
  */
 public class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
-        if (head == null || head.next == null) {
+        if (head == null) {
+            return head;
+        }
+        
+        if (m == n) {
             return head;
         }
         
         ListNode dummy = new ListNode(0);
-        dummy.next = head;
         ListNode prev = dummy;
-        for(int i = 1; i < m; i++) {
+        int total = n - m + 1;
+        while(m-- > 1) {
             prev = prev.next;
         }
         
-        ListNode cur, end;
-        if (prev.next != null && prev.next.next != null) {
-            end = prev.next;
-            cur = prev.next.next;
-        }
-        else {
-            return dummy.next;
-        }
-        
-        int num = n - m;
-        for(int i = 1; i <= n - m; i++) {
+        ListNode end = prev.next;
+        while(total-- > 1) {
+            ListNode cur = end.next;
             end.next = cur.next;
             cur.next = prev.next;
             prev.next = cur;
-            cur = end.next;
         }
         
         return dummy.next;
