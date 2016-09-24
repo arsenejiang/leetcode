77ff75cbda4ef5ba3729548a40cfc3b9678cf90c
arsenejiang <wjiang@outlook.com>
2016-09-24 12:51:23 -0700

    [401. Binary Watch][Accepted]

---
+++ 401-Binary-Watch/solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public List<String> readBinaryWatch(int num) {
+        List<String> res = new ArrayList<String>();
+        for(int h = 0; h < 12; h++) {
+            for(int m = 0; m < 60; m++) {
+                if (Integer.bitCount((h << 6) + m) == num) {
+                    res.add(String.format("%d:%02d", h, m));
+                }
+            }
+        }
+        
+        return res;
+    }
+}
