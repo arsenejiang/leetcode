aedf1a8cc5b9dabbf95b0bef5d7829364bce6396
arsenejiang <wjiang@outlook.com>
2016-09-23 23:46:09 -0700

    [313. Super Ugly Number][Accepted]

--- 313-Super-Ugly-Number/solution.java
+++ 313-Super-Ugly-Number/solution.java
@@ -1,38 +1,22 @@
 public class Solution {
     public int nthSuperUglyNumber(int n, int[] primes) {
-        if (primes == null || primes.length == 0 || n <= 0) {
-            return 0;
-        }
+        int[] ugly = new int[n];
+        int[] idx = new int[primes.length];
         
-        if (n == 1) {
-            return 1;
-        }
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-        Arrays.sort(primes);
-        for(int prime : primes) {
-            pq.offer(prime);
-        }
-        
-        int cur = 0;
-        HashSet<Integer> visited = new HashSet<Integer>();
-        n = n - 1;
-        while(n > 0) {
-            cur = pq.poll();
-            n--;
-            if (n == 0) {
-                return cur;
+        ugly[0] = 1;
+        for(int i = 1; i < n; i++) {
+            ugly[i] = Integer.MAX_VALUE;
+            for(int j = 0; j < primes.length; j++) {
+                ugly[i] = Math.min(ugly[i], primes[j] * ugly[idx[j]]);
             }
             
-            for(int i = 0; i < primes.length; i++) {
-                int next = cur * primes[i];
-                if (!visited.contains(next)) {
-                    pq.offer(next);
-                    visited.add(next);
+            for(int j = 0; j < primes.length; j++) {
+                while(primes[j] * ugly[idx[j]] <= ugly[i]) {
+                    idx[j]++;
                 }
             }
         }
         
-        return cur;
+        return ugly[n - 1];
     }
 }
