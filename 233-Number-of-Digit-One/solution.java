7f12baa85166a192569edfd6833281d66673f69b
arsenejiang <wjiang@outlook.com>
2016-09-25 10:58:37 -0700

    [233. Number of Digit One][Accepted]

--- 233-Number-of-Digit-One/solution.java
+++ 233-Number-of-Digit-One/solution.java
@@ -2,14 +2,7 @@ public class Solution {
     public int countDigitOne(int n) {
         int ones = 0;
         for(long m = 1; m <= n; m *= 10) {
-            // m = 1, (n / m + 8) / 10 to indicate how many counts of 10 (0-9), each one will have 1 DigitOne in the last digit
-            // m = 10, (n / m + 8) / 10 to indicate how many counts of 100 (00-99), each one will have 10 DigitOne in the tens digit
-            // m = 100, (n / m + 8) / 10 to indicate how many counts of 1000 (000-999), each one will have 100 DigitOne in the hundreds digit
-            // (n / m + 8) to separate the cases between (0, 1) and 2. 2 will have the full steak.
             ones += (n / m + 8) / 10 * m;
-            
-            // n / m % 10 == 1 to check if the digit is 1, if the digit is 1
-            // The digitOne count in current digit is controlled by (n % m + 1) which is all the digits on the right side of current digit
             ones += (n / m % 10 == 1) ? n % m + 1 : 0;
         }
         
