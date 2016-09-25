bab441e7e9d7b5a503e042f1aee04f7cec1050a9
arsenejiang <wjiang@outlook.com>
2016-09-25 12:12:45 -0700

    [97. Interleaving String][Accepted]

--- 97-Interleaving-String/solution.java
+++ 97-Interleaving-String/solution.java
@@ -1,25 +1,38 @@
 public class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        int l1 = s1.length();
-        int l2 = s2.length();
+        int m = s1.length();
+        int n = s2.length();
         int l3 = s3.length();
-        if (l1 + l2 != l3) {
+        if (m + n != l3) {
             return false;
         }
         
-        if (l1 == 0) {
-            return s2.equals(s3);
-        }
-        else if (l2 == 0) {
-            return s1.equals(s3);
-        }
-        else {
-            if (s1.charAt(0) == s3.charAt(0) || s2.charAt(0) == s3.charAt(0)) {
-                return (s1.charAt(0) == s3.charAt(0) && isInterleave(s1.substring(1, l1), s2, s3.substring(1, l3))) || (s2.charAt(0) == s3.charAt(0) && isInterleave(s1, s2.substring(1, l2), s3.substring(1, l3)));
-            }
-            else {
-                return false;
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for(int i = 1; i <= m; i++) {
+            if (dp[i-1][0] && s1.charAt(i-1) == s3.charAt(i-1)) {
+                dp[i][0] = true;
             }
         }
+        
+        for(int i = 1; i <= n; i++) {
+            if (dp[0][i-1] && s2.charAt(i-1) == s3.charAt(i-1)) {
+                dp[0][i] = true;
+            }
+        }
+        
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if (dp[i-1][j] && s1.charAt(i-1) == s3.charAt(i + j - 1)) {
+                    dp[i][j] = true;
+                }
+                
+                if (!dp[i][j] && dp[i][j-1] && s2.charAt(j-1) == s3.charAt(i + j - 1)) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        
+        return dp[m][n];
     }
 }
