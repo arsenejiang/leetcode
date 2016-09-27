9b8874da65f48439734669b385fa3900c0f56c5c
arsenejiang <wjiang@outlook.com>
2016-09-27 00:10:24 -0700

    [279. Perfect Squares][Accepted]

--- 279-Perfect-Squares/solution.java
+++ 279-Perfect-Squares/solution.java
@@ -1,28 +1,26 @@
 public class Solution {
     public int numSquares(int n) {
-        int[] dp = new int[n+1];
-        
-        dp[1] = 1;
+        int[] nums = new int[n + 1];
         
         for(int i = 1; i <= n; i++) {
             int square = i * i;
             if (square < n) {
-                dp[square] = 1;
+                nums[square] = 1;
             }
             else if (square == n) {
                 return 1;
             }
             
-            if (dp[i] != 0) {
+            if (nums[i] != 0) {
                 continue;
             }
             
-            dp[i] = Integer.MAX_VALUE;
+            nums[i] = Integer.MAX_VALUE;
             for(int j = 1; j * j <= i; j++) {
-                dp[i] = Math.min(dp[i], 1 + dp[i - j*j]);
+                nums[i] = Math.min(nums[i], 1 + nums[i - j * j]);
             }
         }
         
-        return dp[n];
+        return nums[n];
     }
 }
