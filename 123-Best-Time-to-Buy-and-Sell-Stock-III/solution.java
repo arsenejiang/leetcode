a4f90811bba8eec779e32f10a220ce25f3a61e04
arsenejiang <wjiang@outlook.com>
2016-09-26 22:37:24 -0700

    [123. Best Time to Buy and Sell Stock III][Accepted]

--- 123-Best-Time-to-Buy-and-Sell-Stock-III/solution.java
+++ 123-Best-Time-to-Buy-and-Sell-Stock-III/solution.java
@@ -1,29 +1,14 @@
 public class Solution {
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length <= 1) {
-            return 0;
+        int buy1 = Integer.MIN_VALUE, sell1 = 0;
+        int buy2 = Integer.MIN_VALUE, sell2 = 0;
+        for(int p : prices) {
+            sell2 = Math.max(sell2, buy2 + p);
+            buy2 = Math.max(buy2, sell1 - p);
+            sell1 = Math.max(sell1, buy1 + p);
+            buy1 = Math.max(buy1, -p);
         }
         
-        int max = 0;
-        for(int i = 0; i <= prices.length - 1; i++) {
-            max = Math.max(max, maxProfit(prices, 0, i) + maxProfit(prices, i, prices.length - 1));
-        }
-        
-        return max;
-    }
-    
-    private int maxProfit(int[] prices, int start, int end) {
-        int max = 0;
-        int min = Integer.MAX_VALUE;
-        for(int i = start; i <= end; i++) {
-            if (prices[i] < min) {
-                min = prices[i];
-            }
-            else {
-                max = Math.max(max, prices[i] - min);
-            }
-        }
-        
-        return max;
+        return sell2;
     }
 }
