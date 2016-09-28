4b98a05edd4dbfa60d7fd9a3fb74f44f33301131
arsenejiang <wjiang@outlook.com>
2016-09-28 00:01:50 -0700

    [151. Reverse Words in a String][Accepted]

--- 151-Reverse-Words-in-a-String/solution.java
+++ 151-Reverse-Words-in-a-String/solution.java
@@ -1,32 +1,26 @@
 public class Solution {
     public String reverseWords(String s) {
-        int len = s.length();
-        StringBuilder sb = new StringBuilder();
-        int endIndex = -1;
-        for(int i = len - 1; i >= 0; i--) {
-            if (s.charAt(i) == ' ') {
-                if(endIndex != -1) {
-                    if (sb.length() != 0) {
-                        sb.append(' ');
-                    }
-                    sb.append(s.substring(i + 1, endIndex + 1));
-                    endIndex = -1;
-                }
-            }
-            else {
-                if (endIndex == -1) {
-                    endIndex = i;
-                }
-            }
+        if (s == null || s.length() == 0) {
+            return s;
         }
         
-        if (endIndex != -1) {
-            if (sb.length() != 0) {
-                sb.append(' ');
+        String[] arr = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i = arr.length - 1; i >= 0; i--) {
+            if (isEmptyOrWhiteSpace(arr[i])) {
+                continue;
             }
-            sb.append(s.substring(0, endIndex + 1));
+            
+            if (sb.length() > 0) {
+                sb.append(" ");
+            }
+            sb.append(arr[i]);
         }
         
         return sb.toString();
+    }
+    
+    private boolean isEmptyOrWhiteSpace(String str) {
+        return str.length() == 0 || str.charAt(0) == ' ';
     }
 }
