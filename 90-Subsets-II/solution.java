4de21d6be1a48089257eaa4adecc6e446d01777a
arsenejiang <wjiang@outlook.com>
2016-09-29 00:37:29 -0700

    [90. Subsets II][Accepted]

--- 90-Subsets-II/solution.java
+++ 90-Subsets-II/solution.java
@@ -1,21 +1,23 @@
 public class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> res = new ArrayList();
-        List<Integer> empty = new ArrayList();
-        res.add(empty);
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
         if (nums == null || nums.length == 0) {
             return res;
         }
-        int size = 0;
-        Arrays.sort(nums);
         
+        Arrays.sort(nums);
+        boolean[] used = new boolean[nums.length];
+        List<Integer> empty = new ArrayList<Integer>();
+        res.add(empty);
+        
+        int size = 0;
         for(int i = 0; i < nums.length; i++) {
             int start = (i >= 1 && nums[i] == nums[i-1]) ? size : 0;
             size = res.size();
             for(int j = start; j < size; j++) {
-                List<Integer> newList = new ArrayList(res.get(j));
-                newList.add(nums[i]);
-                res.add(newList);
+                List<Integer> list = new ArrayList<Integer>(res.get(j));
+                list.add(nums[i]);
+                res.add(list);
             }
         }
         
