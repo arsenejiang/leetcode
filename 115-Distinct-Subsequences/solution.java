28bff97140441f8e3e544367d3cb931cef4c274e
arsenejiang <wjiang@outlook.com>
2016-09-28 23:24:14 -0700

    [115. Distinct Subsequences][Runtime Error]

--- 115-Distinct-Subsequences/solution.java
+++ 115-Distinct-Subsequences/solution.java
@@ -5,22 +5,24 @@ public class Solution {
         if (slen < tlen) {
             return 0;
         }
-        else if (slen == tlen) {
-            if (s.equals(t)) {
-                return 1;
-            }
-            else {
-                return 0;
-            }
-        }
         
         int[][] dp = new int[slen + 1][tlen + 1];
-        for(int i = 0; i <= slen; i++) {
+        for(int i = 0; i <= tlen; i++) {
+            dp[0][i] = 0;
+        }
+        
+        for(int i = 1; i <= slen; i++) {
             dp[i][0] = 1;
         }
+        
         for(int i = 1; i <= slen; i++) {
-            for(int j = 1; j <= i && j <= tlen; j++) {
-                dp[i][j] = (s.charAt(i-1) == t.charAt(j-1)) ? dp[i-1][j-1] + dp[i-1][j] : dp[i-1][j];
+            for(int j = 1; j <= i; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }
+                else {
+                    dp[i][j] = dp[i-1][j];
+                }
             }
         }
         
