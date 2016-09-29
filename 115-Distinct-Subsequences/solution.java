4615e287cf52a8fe5593b4b367cae56f3b7f2539
arsenejiang <wjiang@outlook.com>
2016-09-28 23:26:07 -0700

    [115. Distinct Subsequences][Accepted]

--- 115-Distinct-Subsequences/solution.java
+++ 115-Distinct-Subsequences/solution.java
@@ -7,16 +7,12 @@ public class Solution {
         }
         
         int[][] dp = new int[slen + 1][tlen + 1];
-        for(int i = 0; i <= tlen; i++) {
-            dp[0][i] = 0;
-        }
-        
-        for(int i = 1; i <= slen; i++) {
+        for(int i = 0; i <= slen; i++) {
             dp[i][0] = 1;
         }
         
         for(int i = 1; i <= slen; i++) {
-            for(int j = 1; j <= i; j++) {
+            for(int j = 1; j <= i && j <= tlen; j++) {
                 if (s.charAt(i - 1) == t.charAt(j - 1)) {
                     dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
                 }
