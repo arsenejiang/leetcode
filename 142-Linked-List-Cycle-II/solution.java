53435d7391f93f614ce4c020fc9466fd0dcc5faa
arsenejiang <wjiang@outlook.com>
2016-10-01 00:13:48 -0700

    [142. Linked List Cycle II][Time Limit Exceeded]

--- 142-Linked-List-Cycle-II/solution.java
+++ 142-Linked-List-Cycle-II/solution.java
@@ -17,9 +17,12 @@ public class Solution {
         
         ListNode slow = head;
         ListNode fast = head.next;
-        while(fast != null && fast.next != null && slow != fast) {
+        while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
         }
         
         if (slow != fast) {
