ffd9a434b933fe018facecea03477902b1bdb9cf
arsenejiang <wjiang@outlook.com>
2016-09-30 23:55:52 -0700

    [142. Linked List Cycle II][Time Limit Exceeded]

---
+++ 142-Linked-List-Cycle-II/solution.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast != null && fast.next != null && slow != fast) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        if (slow != fast) {
+            return null;
+        }
+        
+        fast = head;
+        while(slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        return slow;
+    }
+}
