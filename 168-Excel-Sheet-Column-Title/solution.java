4bc4dfda98d7e054aa1d32006ae8f9bca7822e09
arsenejiang <wjiang@outlook.com>
2016-09-30 21:31:49 -0700

    [168. Excel Sheet Column Title][Accepted]

--- 168-Excel-Sheet-Column-Title/solution.java
+++ 168-Excel-Sheet-Column-Title/solution.java
@@ -7,10 +7,10 @@ public class Solution {
         StringBuilder sb = new StringBuilder();
         while(n > 0) {
             n--;
-            sb.insert(0, (char)((n % 26) + 'A'));
+            sb.append((char)('A' + n % 26));
             n = n / 26;
         }
         
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
