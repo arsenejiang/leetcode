c5fbc2e9c8068d5a61e4bd64fcad83ce1090e234
arsenejiang <wjiang@outlook.com>
2016-10-02 00:58:39 -0700

    [322. Coin Change][Accepted]

--- 322-Coin-Change/solution.java
+++ 322-Coin-Change/solution.java
@@ -3,31 +3,19 @@ public class Solution {
         if (coins == null || coins.length == 0) {
             return -1;
         }
-        Arrays.sort(coins);
         
         int[] dp = new int[amount + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
-        for(int i = 0; i < coins.length && coins[i] <= amount; i++) {
-            dp[coins[i]] = 1;
-        }
         
         for(int i = 1; i <= amount; i++) {
-            if (dp[i] != Integer.MAX_VALUE) {
-                continue;
-            }
-            
             for(int j = 0; j < coins.length; j++) {
-                if (i - coins[j] >= 0 && dp[i - coins[j]] != -1) {
+                if (i - coins[j] >= 0 && dp[i - coins[j]] != Integer.MAX_VALUE) {
                     dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
                 }
             }
-            
-            if (dp[i] == Integer.MAX_VALUE) {
-                dp[i] = -1;
-            }
         }
         
-        return dp[amount];
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
     }
 }
