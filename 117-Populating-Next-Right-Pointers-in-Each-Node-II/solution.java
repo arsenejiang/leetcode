a167300f6aa053162482b839d91c90bbab5dc240
arsenejiang <wjiang@outlook.com>
2016-10-03 00:06:26 -0700

    [117. Populating Next Right Pointers in Each Node II][Time Limit Exceeded]

--- 117-Populating-Next-Right-Pointers-in-Each-Node-II/solution.java
+++ 117-Populating-Next-Right-Pointers-in-Each-Node-II/solution.java
@@ -43,7 +43,7 @@ public class Solution {
                 cur = cur.next;
             }
             
-            if (nextlevel == null) {
+            if (nextLevel == null) {
                 return;
             }
             else {
