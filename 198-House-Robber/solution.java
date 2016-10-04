44de32e3e5768f9308f44a030cd7fc2d5e8661c7
arsenejiang <wjiang@outlook.com>
2016-10-04 00:42:32 -0700

    [198. House Robber][Compile Error]

--- 198-House-Robber/solution.java
+++ 198-House-Robber/solution.java
@@ -4,14 +4,15 @@ public class Solution {
             return 0;
         }
         
-        int len = nums.length;
-        int[] dp = new int[len + 1];
+        int[] dp = new int[nums.length + 1];
         dp[0] = 0;
         dp[1] = nums[0];
-        for(int i = 2; i <= len; i++) {
-            dp[i] = Math.max(dp[i - 2] + nums[i - 1], dp[i-1]);
+        int max = dp[1];
+        for(int i = 1; i < nums.length; i++) {
+            dp[i + 1] = Math.max(nums[i] + dp[i-1]) + dp[i];
+            max = Math.max(dp[i + 1], max);
         }
         
-        return dp[len];
+        return max;
     }
 }
