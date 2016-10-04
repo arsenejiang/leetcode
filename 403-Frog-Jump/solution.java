0639ba61e13555d51cfe4a6d067e43585a9f5eed
arsenejiang <wjiang@outlook.com>
2016-10-03 23:27:40 -0700

    [403. Frog Jump][Accepted]

--- 403-Frog-Jump/solution.java
+++ 403-Frog-Jump/solution.java
@@ -4,37 +4,32 @@ public class Solution {
             return true;
         }
         
-        int len = stones.length;
-        int[] gap = new int[len - 1];
-        for(int i = 0; i < gap.length; i++) {
-            gap[i] = stones[i + 1] - stones[i];
-        }
-        
-        if (gap[0] != 1) {
+        if (stones[1] - stones[0] != 1) {
             return false;
         }
         
-        List<Integer>[] dp = (List<Integer>[])new ArrayList[gap.length];
-        for(int i = 0; i < gap.length; i++) {
-            dp[i] = new ArrayList<Integer>();
+        HashMap<Integer, Set<Integer>> map = new HashMap<Integer, Set<Integer>>();
+        for(int stone : stones) {
+            map.put(stone, new HashSet<Integer>());
         }
         
-        dp[0].add(1);
+        map.get(stones[1]).add(1);
         
-        for(int i = 1; i < gap.length; i++) {
-            int sum = gap[i];
-            for(int j = i - 1; j >= 0; j--) {
-                for(int prevVal : dp[j]) {
-                    if (sum >= prevVal - 1 && sum <= prevVal + 1) {
-                        dp[i].add(sum);
+        for(int i = 2; i < stones.length; i++) {
+            for(int val : map.get(stones[i-1])) {
+                for(int j = Math.max(1, val - 1); j <= val + 1; j++) {
+                    int nextStone = stones[i-1] + j;
+                    if (nextStone == stones[stones.length - 1]) {
+                        return true;
+                    }
+                    
+                    if (map.containsKey(nextStone)) {
+                        map.get(nextStone).add(j);
                     }
                 }
-                
-                sum += gap[j];
             }
         }
         
-        
-        return dp[gap.length - 1].size() != 0;
+        return map.get(stones[stones.length - 1]).size() > 0;
     }
 }
