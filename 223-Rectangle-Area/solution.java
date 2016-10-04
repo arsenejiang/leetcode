c38687f2503ff410647fa284379339a42a8b3d68
arsenejiang <wjiang@outlook.com>
2016-10-04 00:53:12 -0700

    [223. Rectangle Area][Compile Error]

--- 223-Rectangle-Area/solution.java
+++ 223-Rectangle-Area/solution.java
@@ -1,21 +1,15 @@
 public class Solution {
     public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-        // totalArea = area1 + area2 - overlap
-        int area = (C - A) * (D - B) + (G - E) * (H - F); // area1 + area2
-        
-        // overlap
-        // bottom left (I, J)
-        // top right (K, L)
+        int totalArea = (C - A) * (D - B) + (G - E) * (H - F);
         int I = Math.max(A, E);
         int J = Math.max(B, F);
-        int K = Math.min(C, G);
-        int L = Math.min(D, H);
-        
-        // overlap exists
-        if (K > I && L > J) {
-            area -= (K - I) * (L - J);
+        int K = Math.max(C, G);
+        int L = Math.max(D, H);
+        if (k > I && L > J) {
+            totalArea -= (K - I) * (L - J);
         }
         
-        return area;
+        return totalArea;
+        
     }
 }
