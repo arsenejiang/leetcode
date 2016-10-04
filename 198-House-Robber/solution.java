e1f0626336e0990e2b6cee56a91ad34d8046804a
arsenejiang <wjiang@outlook.com>
2016-10-04 00:42:48 -0700

    [198. House Robber][Accepted]

--- 198-House-Robber/solution.java
+++ 198-House-Robber/solution.java
@@ -9,7 +9,7 @@ public class Solution {
         dp[1] = nums[0];
         int max = dp[1];
         for(int i = 1; i < nums.length; i++) {
-            dp[i + 1] = Math.max(nums[i] + dp[i-1]) + dp[i];
+            dp[i + 1] = Math.max(nums[i] + dp[i-1], dp[i]);
             max = Math.max(dp[i + 1], max);
         }
         
