74d22a91c8c2f29c38d7208e8bb59edac9da5770
arsenejiang <wjiang@outlook.com>
2016-10-03 23:30:28 -0700

    [226. Invert Binary Tree][Accepted]

--- 226-Invert-Binary-Tree/solution.java
+++ 226-Invert-Binary-Tree/solution.java
@@ -13,11 +13,13 @@ public class Solution {
             return root;
         }
         
-        TreeNode left = invertTree(root.left);
-        TreeNode right = invertTree(root.right);
-        root.left = right;
-        root.right = left;
+        if (root.left == null && root.right == null) {
+            return root;
+        }
         
+        TreeNode temp = invertTree(root.left);
+        root.left = invertTree(root.right);
+        root.right = temp;
         return root;
     }
 }
