6a4a524a024756b6cdf309485ed85c76c93c536e
arsenejiang <wjiang@outlook.com>
2016-10-06 21:16:25 -0700

    [30. Substring with Concatenation of All Words][Accepted]

--- 30-Substring-with-Concatenation-of-All-Words/solution.java
+++ 30-Substring-with-Concatenation-of-All-Words/solution.java
@@ -1,27 +1,61 @@
 public class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        List<String> res = new ArrayList<String>();
-        if (s == null || s.length() == 0 || words == null || words.length == 0 || words[0].length() == 0) {
+        List<Integer> res = new ArrayList();
+        if (s == null || s.length() == 0 || words == null || words.length == 0) {
             return res;
         }
         
-        HashMap<String, Integer> wmap = new HashMap<String, Integer>();
-        int wlen = words[0].length();
-        int tlen = 0;
-        int slen = s.length();
-        for(String word : words) {
-            tlen += wlen;
-            if (!wmap.containsKey(word)) {
-                wmap.put(word, 1);
+        int sLen = s.length();
+        int wLen = words[0].length();
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        for(String w : words) {
+            if (map.containsKey(w)) {
+                map.put(w, map.get(w) + 1);
             }
             else {
-                wmap.put(word, wmap.get(word) + 1);
+                map.put(w, 1);
             }
         }
         
-        for(int i = 0; i < wlen; i++) {
-            int left = i;
-            for(int j = i; )
+        for(int i = 0; i < wLen; i++) {
+            HashMap<String, Integer> covered = new HashMap<String, Integer>();
+            int start = i;
+            int count = 0;
+            for(int j = i; j <= sLen - wLen; j += wLen) {
+                String sub = s.substring(j, j + wLen);
+                if (map.containsKey(sub)) {
+                    if (covered.containsKey(sub)) {
+                        covered.put(sub, covered.get(sub) + 1);
+                    }   
+                    else {
+                        covered.put(sub, 1);
+                    }
+                    count++;
+                    
+                    while(map.get(sub) < covered.get(sub)) {
+                        String temp = s.substring(start, start + wLen);
+                        covered.put(temp, covered.get(temp) - 1);
+                        count--;
+                        start = start + wLen;
+                    }
+                    
+                    if (count == words.length) {
+                        res.add(start);
+                        String temp = s.substring(start, start + wLen);
+                        covered.put(temp, covered.get(temp) - 1);
+                        count--;
+                        start = start + wLen;
+                    }
+                }
+                else {
+                    start = j + wLen;
+                    covered.clear();
+                    count = 0;
+                }
+            }
         }
+        
+        return res;
+        
     }
 }
