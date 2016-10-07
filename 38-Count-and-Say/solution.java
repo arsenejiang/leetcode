d7629013fcca3a52dd14ff2dc29f5b8b7aea3a67
arsenejiang <wjiang@outlook.com>
2016-10-06 22:08:11 -0700

    [38. Count and Say][Runtime Error]

--- 38-Count-and-Say/solution.java
+++ 38-Count-and-Say/solution.java
@@ -7,11 +7,15 @@ public class Solution {
         StringBuilder[] dp = new StringBuilder[n + 1];
         dp[1] = new StringBuilder("1");
         for(int i = 2; i <= n; i++) {
-            char prev = dp[i - 1].charAt(0);
+            char prev = '0';
             int count = 1;
             for(int j = 0; j < dp[i - 1].length(); j++) {
                 char c = dp[i - 1].charAt(j);
-                if (c != prev) {
+                if (j == 0) {
+                    prev = c;
+                    count = 1;
+                }
+                else if (c != prev) {
                     dp[i].append(String.valueOf(count));
                     dp[i].append(prev);
                     prev = c;
