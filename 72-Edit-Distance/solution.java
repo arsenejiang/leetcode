c46af4d83b502c434432f9162ef7d668781998b4
arsenejiang <wjiang@outlook.com>
2016-10-08 15:20:34 -0700

    [72. Edit Distance][Accepted]

--- 72-Edit-Distance/solution.java
+++ 72-Edit-Distance/solution.java
@@ -1,28 +1,27 @@
 public class Solution {
     public int minDistance(String word1, String word2) {
-        int w1 = word1.length();
-        int w2 = word2.length();
-        int[][] dp = new int[w1+1][w2+1];
-        dp[0][0] = 0;
-        for(int i = 1; i < w1 + 1; i++) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for(int i = 0; i <= m; i++) {
             dp[i][0] = i;
         }
         
-        for(int j = 1; j < w2 + 1; j++) {
+        for(int j = 1; j <= n; j++) {
             dp[0][j] = j;
         }
         
-        for(int i = 1; i < w1 + 1; i++) {
-            for(int j = 1; j < w2 + 1; j++) {
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
                 if (word1.charAt(i-1) == word2.charAt(j-1)) {
                     dp[i][j] = dp[i-1][j-1];
                 }
                 else {
-                    dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1;
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j-1], dp[i][j-1]), dp[i-1][j]) + 1;
                 }
             }
         }
         
-        return dp[w1][w2];
+        return dp[m][n];
     }
 }
