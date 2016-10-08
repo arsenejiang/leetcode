5b07556929c4493a5dc1e0a3941519c1ab429ac8
arsenejiang <wjiang@outlook.com>
2016-10-07 18:41:01 -0700

    [41. First Missing Positive][Time Limit Exceeded]

--- 41-First-Missing-Positive/solution.java
+++ 41-First-Missing-Positive/solution.java
@@ -3,26 +3,29 @@ public class Solution {
         if (nums == null || nums.length == 0) {
             return 1;
         }
-
+        
         for(int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0 && nums[i] <= nums.length) {
+            if (nums[i] <= 0) {
+                continue;
+            }
+            else if (nums[i] >= 1 && nums[i] <= nums.length && nums[i] != i + 1) {
                 swap(nums, i, nums[i] - 1);
                 i--;
             }
         }
-
+        
         for(int i = 0; i < nums.length; i++) {
             if (nums[i] != i + 1) {
                 return i + 1;
             }
         }
-
+        
         return nums.length + 1;
     }
     
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
