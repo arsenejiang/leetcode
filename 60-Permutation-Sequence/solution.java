8ad6514aaba59732b3b816a918abdb90fe98a773
arsenejiang <wjiang@outlook.com>
2016-10-08 00:12:09 -0700

    [60. Permutation Sequence][Runtime Error]

--- 60-Permutation-Sequence/solution.java
+++ 60-Permutation-Sequence/solution.java
@@ -1,42 +1,31 @@
 public class Solution {
-    private int[] factorial;
-    
     public String getPermutation(int n, int k) {
-        if (n <= 0) {
-            return "";
-        }
-        
-        factorial = new int[n+1];
-        factorial[0] = 1;
-        for(int i = 1; i < n+1; i++) {
-            factorial[i] = i*factorial[i-1];
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        List<Integer> list = new ArrayList();
+        StringBuilder cur = new StringBuilder();
+        List<Integer> nums = new ArrayList<Integer>();
         for(int i = 1; i <= n; i++) {
-            list.add(i);
+            nums.add(i);
         }
-        sb.append(helper(list, k - 1));
-        return sb.toString();
+        helper(nums, cur, k);
+        return cur.toString();
     }
     
-    private String helper(List<Integer> candidates, int k) {
-        int size = candidates.size();
-        if (size == 0) {
-            return "";
+    private void helper(List<Integer> nums, StringBuilder cur, int k) {
+        if (k == 1) {
+            for(int i : nums) {
+                cur.append(i);
+            }
+            return;
         }
         
-        // size of each group
-        int stepLength = factorial[size]/size; 
+        int total = 1;
+        for(int i = nums.size(); i >= 1; i--) {
+            total *= i;
+        }
         
-        // group index
-        int index = k / stepLength;
-        
-        // leftover numbers
-        int left = k - index * stepLength;
-        
-        int val = candidates.remove(index);
-        return val + helper(candidates, left);
+        int bucketSize = total / nums.size();
+        int index = k / bucketSize;
+        cur.append(nums.get(index));
+        nums.remove(index);
+        helper(nums, cur, k - index * bucketSize);
     }
 }
