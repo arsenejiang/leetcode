919731c4d8d7d5d490fc4897e220a9415d2578b6
arsenejiang <wjiang@outlook.com>
2016-10-07 23:15:26 -0700

    [48. Rotate Image][Accepted]

--- 48-Rotate-Image/solution.java
+++ 48-Rotate-Image/solution.java
@@ -5,7 +5,7 @@ public class Solution {
         }
         
         int n = matrix.length;
-        for(int i = 0; i <= n / 2; i++) {
+        for(int i = 0; i < n / 2; i++) {
             for(int j = i; j < n - 1 - i; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[n-1-j][i];
