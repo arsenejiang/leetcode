47e591c8ed455d5bd806c84d4a06180a8f879bec
arsenejiang <wjiang@outlook.com>
2016-10-07 18:49:59 -0700

    [41. First Missing Positive][Time Limit Exceeded]

--- 41-First-Missing-Positive/solution.java
+++ 41-First-Missing-Positive/solution.java
@@ -8,7 +8,7 @@ public class Solution {
             if (nums[i] <= 0) {
                 continue;
             }
-            else if (nums[i] >= 1 && nums[i] <= nums.length && nums[i] != i + 1) {
+            else if (nums[i] > i + 1 && nums[i] <= nums.length) {
                 swap(nums, i, nums[i] - 1);
                 i--;
             }
