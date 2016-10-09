1c607643cf45532ca795243b3cd60a77b22aa67c
arsenejiang <wjiang@outlook.com>
2016-10-09 14:29:03 -0700

    [187. Repeated DNA Sequences][Accepted]

--- 187-Repeated-DNA-Sequences/solution.java
+++ 187-Repeated-DNA-Sequences/solution.java
@@ -1,30 +1,45 @@
 public class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> res = new ArrayList();
-        if (s == null || s.length() < 10) {
+        List<String> res = new ArrayList<String>();
+        if (s == null || s.length() <= 9) {
             return res;
         }
         
-        int len = s.length();
-        Map<Character, Integer> map = new HashMap();
-        map.put('A', 0);
-        map.put('C', 1);
-        map.put('G', 2);
-        map.put('T', 3);
-        Set<Integer> words = new HashSet();
-        Set<Integer> doubleWords = new HashSet();
-        int max = 0xFFFFF;
-        int hash = 0;
-        for(int i = 0; i < len; i++) {
-            if (i < 9) {
-                hash = (hash << 2) + map.get(s.charAt(i));
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i <= s.length() - 10; i++) {
+            int val = strToInt(s.substring(i, i + 10));
+            if (map.containsKey(val)) {
+                map.put(val, map.get(val) + 1);
+                if (map.get(val) == 2) {
+                    res.add(s.substring(i, i + 10));
+                }
             }
             else {
-                hash = (hash << 2) + map.get(s.charAt(i));
-                hash = hash & max;
-                if (!words.add(hash) && doubleWords.add(hash)) {
-                    res.add(s.substring(i - 9, i + 1));
-                }
+                map.put(val, 1);
+            }
+        }
+        
+        return res;
+    }
+    
+    private int strToInt(String s) {
+        int res = 0;
+        for(int i = 0; i < 10; i++) {
+            char c = s.charAt(i);
+            res = res << 2;
+            switch (c) {
+                case 'A':
+                    res = res;
+                    break;
+                case 'C':
+                    res = res + 1;
+                    break;
+                case 'G':
+                    res = res + 2;
+                    break;
+                case 'T':
+                    res = res + 3;
+                    break;
             }
         }
         
