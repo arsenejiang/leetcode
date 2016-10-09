07cc15608674a16549ce688483cfdf8c7b834295
arsenejiang <wjiang@outlook.com>
2016-10-09 13:33:15 -0700

    [154. Find Minimum in Rotated Sorted Array II][Runtime Error]

--- 154-Find-Minimum-in-Rotated-Sorted-Array-II/solution.java
+++ 154-Find-Minimum-in-Rotated-Sorted-Array-II/solution.java
@@ -1,20 +1,42 @@
 public class Solution {
     public int findMin(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int len = nums.length;
+        if (len == 1) {
+            return nums[0];
+        }
+        else if (len == 2) {
+            return Math.min(nums[0], nums[1]);
+        }
+        
         int left = 0;
-        int right = nums.length - 1;
-        while(left < right) {
+        int right = len - 1;
+        while(left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] < nums[right]) {
-                right = mid;
+            int midLeft = (mid - 1) % len;
+            int midRight = (mid + 1) % len;
+            if (nums[mid] < nums[midLeft] && nums[mid] < nums[midRight]) {
+                return nums[mid];
             }
-            else if (nums[mid] > nums[right]) {
-                left = mid + 1;
+            else if (nums[left] < nums[mid]) {
+                if (nums[mid] < nums[right]) {
+                    return nums[left];
+                }
+                else {
+                    left = mid + 1;
+                }
+            }
+            else if (nums[left] > nums[mid]) {
+                right = mid - 1;
             }
             else {
-                right--;
+                left++;
             }
         }
         
-        return nums[left];
+        return 0;
     }
 }
