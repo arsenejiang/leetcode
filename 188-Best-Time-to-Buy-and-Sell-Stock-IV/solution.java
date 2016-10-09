7e5d733505546b9b60943b9987fa782be897412b
arsenejiang <wjiang@outlook.com>
2016-10-09 15:42:45 -0700

    [188. Best Time to Buy and Sell Stock IV][Accepted]

--- 188-Best-Time-to-Buy-and-Sell-Stock-IV/solution.java
+++ 188-Best-Time-to-Buy-and-Sell-Stock-IV/solution.java
@@ -1,12 +1,12 @@
 public class Solution {
     public int maxProfit(int k, int[] prices) {
-        if (prices == null || prices.length <= 1 || k <= 0) {
+        if (k == 0 || prices == null || prices.length <= 1) {
             return 0;
         }
         
         int len = prices.length;
-        int maxProfit = 0;
         if (k >= len / 2) {
+            int maxProfit = 0;
             for(int i = 1; i < len; i++) {
                 if (prices[i] > prices[i-1]) {
                     maxProfit += prices[i] - prices[i-1];
@@ -16,11 +16,11 @@ public class Solution {
         }
         
         int[][] dp = new int[k+1][len];
-        for(int i = 1; i < k + 1; i++) {
-            int tmpMax = -prices[0];
+        for(int i = 1; i <= k; i++) {
+            int localMax = dp[i-1][0] - prices[0];
             for(int j = 1; j < len; j++) {
-                dp[i][j] = Math.max(dp[i][j - 1], prices[j] + tmpMax);
-                tmpMax = Math.max(tmpMax, dp[i-1][j] - prices[j]);
+                dp[i][j] = Math.max(dp[i][j-1], prices[j] + localMax);
+                localMax = Math.max(localMax, dp[i-1][j] - prices[j]);
             }
         }
         
