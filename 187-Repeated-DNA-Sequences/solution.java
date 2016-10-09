d7e20386ad80df7849fb108f9cd7e1a59fc29515
arsenejiang <wjiang@outlook.com>
2016-10-09 14:36:08 -0700

    [187. Repeated DNA Sequences][Accepted]

--- 187-Repeated-DNA-Sequences/solution.java
+++ 187-Repeated-DNA-Sequences/solution.java
@@ -1,5 +1,6 @@
 public class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
+        /*
         List<String> res = new ArrayList<String>();
         if (s == null || s.length() <= 9) {
             return res;
@@ -16,6 +17,29 @@ public class Solution {
             }
             else {
                 map.put(val, 1);
+            }
+        }
+        
+        return res;
+        */
+        List<String> res = new ArrayList();
+        if (s == null || s.length() < 10) {
+            return res;
+        }
+        
+        int len = s.length();
+        Map<String, Integer> map = new HashMap();
+        for(int i = 0; i <= len - 10; i++) {
+            String sub = s.substring(i, i + 10);
+            if (map.containsKey(sub)) {
+                int count = map.get(sub) + 1;
+                map.put(sub, count);
+                if (count == 2) {
+                    res.add(sub);
+                }
+            }
+            else {
+                map.put(sub, 1);
             }
         }
         
@@ -45,4 +69,6 @@ public class Solution {
         
         return res;
     }
+    
+    
 }
