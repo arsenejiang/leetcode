968956abd7f128ab9494fd2d50b4d3f719fafafe
arsenejiang <wjiang@outlook.com>
2016-10-08 23:47:43 -0700

    [113. Path Sum II][Accepted]

--- 113-Path-Sum-II/solution.java
+++ 113-Path-Sum-II/solution.java
@@ -9,22 +9,26 @@
  */
 public class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
-        List<List<Integer>> res = new ArrayList();
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
         if (root == null) {
             return res;
         }
         
-        List<Integer> cur = new ArrayList();
-        helper(root, sum, res, cur);
+        List<Integer> cur = new ArrayList<Integer>();
+        helper(root, sum, cur, res);
         return res;
     }
     
-    private void helper(TreeNode root, int sum, List<List<Integer>> res, List<Integer> cur) {
-        if(root.left == null && root.right == null) {
-            if (root.val == sum) {
-                cur.add(root.val);
-                res.add(new ArrayList(cur));
-                cur.remove(cur.size() - 1);
+    private void helper(TreeNode root, int sum, List<Integer> cur, List<List<Integer>> res) {
+        if (root == null) {
+            return;
+        }
+        
+        if (root.left == null && root.right == null) {
+            if (sum == root.val) {
+                List<Integer> list = new ArrayList<Integer>(cur);
+                list.add(root.val);
+                res.add(list);
                 return;
             }
             else {
@@ -32,21 +36,15 @@ public class Solution {
             }
         }
         
-        if (root.left == null) {
-            cur.add(root.val);
-            helper(root.right, sum - root.val, res, cur);
-            cur.remove(cur.size() - 1);
+        cur.add(root.val);
+        if (root.left != null) {
+            helper(root.left, sum - root.val, cur, res);
         }
-        else if (root.right == null) {
-            cur.add(root.val);
-            helper(root.left, sum - root.val, res, cur);
-            cur.remove(cur.size() - 1);
+        
+        if (root.right != null) {
+            helper(root.right, sum - root.val, cur, res);
         }
-        else {
-            cur.add(root.val);
-            helper(root.left, sum - root.val, res, cur);
-            helper(root.right, sum - root.val, res, cur);
-            cur.remove(cur.size() - 1);
-        }
+        
+        cur.remove(cur.size() - 1);
     }
 }
