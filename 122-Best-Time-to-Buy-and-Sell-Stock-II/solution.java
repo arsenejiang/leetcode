abfdeb5e814e541a4a5311abb68433b597e6562c
arsenejiang <wjiang@outlook.com>
2016-10-09 00:02:22 -0700

    [122. Best Time to Buy and Sell Stock II][Accepted]

--- 122-Best-Time-to-Buy-and-Sell-Stock-II/solution.java
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II/solution.java
@@ -5,7 +5,6 @@ public class Solution {
         }
         
         int maxProfit = 0;
-        int prev = prices[0];
         for(int i = 1; i < prices.length; i++) {
             if (prices[i] > prices[i-1]) {
                 maxProfit += prices[i] - prices[i-1];
