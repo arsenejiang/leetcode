f70fa66fc97e2d6ff7994cb68fcbd0a7b1255f07
arsenejiang <wjiang@outlook.com>
2016-10-10 00:12:50 -0700

    [304. Range Sum Query 2D - Immutable][Compile Error]

--- 304-Range-Sum-Query-2D---Immutable/solution.java
+++ 304-Range-Sum-Query-2D---Immutable/solution.java
@@ -1,18 +1,18 @@
 public class NumMatrix {
-    private int[][] dp;
+    private int[][] sum;
     public NumMatrix(int[][] matrix) {
         int m = matrix.length;
-        int n = m > 0 ? matrix[0].length : 0;
-        dp = new int[m+1][n+1];
+        int n = matrix[0].length;
+        sum = new int[m + 1][n + 1];
         for(int i = 1; i < m + 1; i++) {
             for(int j = 1; j < n + 1; j++) {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1] + matrix[i-1][j-1] - dp[i-1][j-1];
+                sum[i][j] = sum[i-1][j] + sum[i][j-1] + matrix[i - 1][j - 1] - sum[i-1][j-1];
             }
         }
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+        return sum[row2 + 1][col2 + 1] - sum[row1][col2 + 1] - sum[row + 1][col1] + sum[row1][col1];
     }
 }
 
