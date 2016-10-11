468a4442cbb0633c558f7cacc7f8d0fbfc05b46c
arsenejiang <wjiang@outlook.com>
2016-10-10 23:03:07 -0700

    [307. Range Sum Query - Mutable][Compile Error]

--- 307-Range-Sum-Query---Mutable/solution.java
+++ 307-Range-Sum-Query---Mutable/solution.java
@@ -4,23 +4,29 @@ public class NumArray {
     int n;
     
     public NumArray(int[] nums) {
-        this.nums = new int[nums.length + 1];
-        this.BIT = new int[nums.length + 1];
-        for(int i = 1; i <= nums.length; i++) {
-            update(i, nums[i-1]);
+        this.nums = nums;
+        n = nums.length + 1;
+        this.BIT = new int[n];
+        for(int i = 0; i < nums.length; i++) {
+            init(i, nums[i]);
         }
     }
 
-    void update(int index, int val) {
-        int diff = val - nums[index ];
-        for(int i = index; i < nums.length; i += (i & -i)) {
-            BIT[i] += diff;
-        }
-        nums[index] = val;
+    void update(int i, int val) {
+        int diff = val - nums[i];
+        nums[i] = val;
+        init(i, diff);
     }
 
     public int sumRange(int i, int j) {
         return getSum(j + 1) - getSum(i);
+    }
+    
+    private int init(int index, int val) {
+        index++;
+        for(int i = index; i < n; i += (i & -i)) {
+            BIT[i] += val;
+        }
     }
     
     private int getSum(int index) {
@@ -29,10 +35,6 @@ public class NumArray {
             sum += BIT[i];
         }
         return sum;
-    }
-    
-    private int lowbit(int i) {
-        return (i & -i);
     }
 }
 
