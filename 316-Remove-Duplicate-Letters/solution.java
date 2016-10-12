3f8a417ab1547be0c279637b9e955ebe3d2a7af6
arsenejiang <wjiang@outlook.com>
2016-10-11 22:40:55 -0700

    [316. Remove Duplicate Letters][Accepted]

--- 316-Remove-Duplicate-Letters/solution.java
+++ 316-Remove-Duplicate-Letters/solution.java
@@ -1,4 +1,6 @@
 public class Solution {
+    /*
+    =
     public String removeDuplicateLetters(String s) {
         if (s == null || s.length() == 0) {
             return "";
@@ -20,5 +22,39 @@ public class Solution {
         }
         
         return s.charAt(pos) + removeDuplicateLetters(s.substring(pos + 1).replaceAll("" + s.charAt(pos), ""));
+    }
+    */
+    
+    public String removeDuplicateLetters(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        
+        int[] count = new int[26];
+        for(int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+        
+        Stack<Character> stack = new Stack<Character>();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            count[c - 'a']--;
+            if (stack.search(c) >= 1) {
+                continue;
+            }
+            
+            while (!stack.isEmpty() && stack.peek() > c && count[stack.peek() - 'a'] > 0) {
+                stack.pop();
+            }
+            
+            stack.push(c);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        
+        return sb.reverse().toString();
     }
 }
